946b77eeabf7bfb6106bdebba941e9ac2053f2f6
Jerjou Cheng <jerjou@google.com>
2015-08-31 12:57:16 -0700

    Add comments describing what's happening.

--- lib/cloud_storage/list_buckets.rb
+++ lib/cloud_storage/list_buckets.rb
@@ -28,14 +28,18 @@ module Samples
       Storage = Google::Apis::StorageV1
 
       def list_buckets project_id
-        # Create Storage service that uses Application Default Credentials
+        # Create the storage service object, used to access the storage api.
         storage = Storage::StorageService.new
+        # Have the service object use the application default credentials to
+        # auth, which infers credentials from the environment.
         storage.authorization = Google::Auth.get_application_default(
+          # Set the credentials to have a readonly scope to the storage service.
           Storage::AUTH_DEVSTORAGE_READ_ONLY
         )
 
         # Make the api call to list buckets owned by the default credentials.
         storage.list_buckets(project_id).items.each do |bucket|
+          # Print out each bucket name.
           puts bucket.name
         end
       end
