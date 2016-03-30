ec8cf3d09d39009432680ed793bc6a963fdfd54a
remi Taylor <remily@google.com>
2016-03-29 17:44:39 -0700

    Add region tag for Hello World app content

--- appengine/hello_world/app.rb
+++ appengine/hello_world/app.rb
@@ -12,8 +12,10 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+# [START app]
 require "sinatra"
 
 get "/" do
   "Hello world!"
 end
+# [END app]
