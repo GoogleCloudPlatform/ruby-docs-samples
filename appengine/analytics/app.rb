4dc065a92d72022921303ecaa1fc4f29e4fd0e7a
remi Taylor <remily@google.com>
2016-04-22 12:34:47 -0700

    Add doc region tags for Flex GA sample

--- appengine/analytics/app.rb
+++ appengine/analytics/app.rb
@@ -12,6 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+# [START all]
 require "sinatra"
 require "net/http"
 
@@ -41,3 +42,4 @@ get "/" do
 
   "Event tracked."
 end
+# [END all]
