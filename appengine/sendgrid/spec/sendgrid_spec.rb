5d448935f143ca1a6e3138b4beb28564d2986154
remi Taylor <remily@google.com>
2016-04-22 17:55:35 -0700

    Fix type in SendGrid test description

--- appengine/sendgrid/spec/sendgrid_spec.rb
+++ appengine/sendgrid/spec/sendgrid_spec.rb
@@ -19,7 +19,7 @@ require "capybara/poltergeist"
 
 Capybara.current_driver = :poltergeist
 
-RSpec.describe "Cloud Storage on Google App Engine", type: :feature do
+RSpec.describe "SendGrid on Google App Engine", type: :feature do
   before :all do
     @url = E2E.url
   end
