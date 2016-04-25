51cf271e96f80dd190329bc0d9d5d7f52d9ced5c
remi Taylor <remily@google.com>
2016-04-25 16:59:59 -0700

    Add sample reading GCE Metadata server from GAE flex

---
+++ appengine/metadata_server/spec/metadata_server_spec.rb
@@ -0,0 +1,34 @@
+# Copyright 2016 Google, Inc
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+require File.expand_path("../../../../spec/e2e", __FILE__)
+require "rspec"
+require "capybara/rspec"
+require "capybara/poltergeist"
+
+Capybara.current_driver = :poltergeist
+
+RSpec.describe "Metadata server on Google App Engine", type: :feature do
+  before :all do
+    @url = E2E.url
+  end
+
+  it "displays IP address read from metadata server" do
+    visit @url
+
+    expect(page.body).to match(
+      /External IP: \d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}/
+    )
+  end
+end
