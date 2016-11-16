9acdd156e98d43c333d98b7e5d0c413de17c41a4
remi Taylor <remily@google.com>
2016-11-16 15:12:27 -0800

    require "google/cloud/datastore"

--- datastore/sample.rb
+++ datastore/sample.rb
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-require "google/cloud"
+require "google/cloud/datastore"
 
 def incomplete_key
   datastore = Google::Cloud::Datastore.new
