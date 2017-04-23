276756f717d28d5d05fd341e15eef9af03d6fadc
Frank Natividad <franknatividad@google.com>
2017-04-22 18:18:21 -0700

    Adding boilerplate example to config/routes.rb

--- appengine/rails-cloudsql-mysql/config/routes.rb
+++ appengine/rails-cloudsql-mysql/config/routes.rb
@@ -4,3 +4,14 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'cat_friends#index'
 end
+
+=begin
+# [START boilerplate]
+Rails.application.routes.draw do
+  get 'cat_friends/index'
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+end
+# [END boilerplate]
+=end
+
