855f03236f395218a54a8c31a615ae61cd0484ed
Frank Natividad <franknatividad@google.com>
2017-04-22 18:20:47 -0700

    Added cat_route region tag

--- appengine/rails-cloudsql-mysql/config/routes.rb
+++ appengine/rails-cloudsql-mysql/config/routes.rb
@@ -1,9 +1,11 @@
+# [START cat_routes]
 Rails.application.routes.draw do
   get 'cat_friends/index'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'cat_friends#index'
 end
+# [END cat_routes]
 
 =begin
 # [START boilerplate]
