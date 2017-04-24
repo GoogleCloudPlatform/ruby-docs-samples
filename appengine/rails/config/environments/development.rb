c65ede82ec57a398c82a571c1d4efb8e50956ff6
Frank Natividad <franknatividad@google.com>
2017-04-24 15:02:55 -0700

    Removing action cable mailer and active record from hello-world rails

--- appengine/rails/config/environments/development.rb
+++ appengine/rails/config/environments/development.rb
@@ -26,16 +26,8 @@ Rails.application.configure do
     config.cache_store = :null_store
   end
 
-  # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = false
-
-  config.action_mailer.perform_caching = false
-
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
-
-  # Raise an error on page load if there are pending migrations.
-  config.active_record.migration_error = :page_load
 
   # Debug mode disables concatenation and preprocessing of assets.
   # This option may cause significant delays in view rendering with a large
--- appengine/rails/config/initializers/new_framework_defaults.rb
+++ appengine/rails/config/initializers/new_framework_defaults.rb
@@ -14,9 +14,6 @@ Rails.application.config.action_controller.forgery_protection_origin_check = tru
 # Previous versions had false.
 ActiveSupport.to_time_preserves_timezone = true
 
-# Require `belongs_to` associations by default. Previous versions had false.
-Rails.application.config.active_record.belongs_to_required_by_default = true
-
 # Do not halt callback chains when a callback returns false. Previous versions had true.
 ActiveSupport.halt_callback_chains_on_return_false = false
 
