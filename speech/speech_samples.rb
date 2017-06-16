6c43bcaf9f9e4752ba6da3d586037bc751e69493
Frank Natividad <franknatividad@google.com>
2017-06-16 16:06:03 -0700

    Adding streaming sample for speech

--- speech/speech_samples.rb
+++ speech/speech_samples.rb
@@ -100,6 +100,42 @@ def speech_async_recognize_gcs project_id:, storage_path:
 # [END speech_async_recognize_gcs]
 end
 
+def speech_streaming_recognize project_id:, audio_file_path:
+# [START speech_streaming]
+  # project_id      = "Your Google Cloud project ID"
+  # audio_file_path = "Path to file on which to perform speech recognition"
+
+  require "google/cloud/speech"
+
+  speech = Google::Cloud::Speech.new project: project_id
+  stream = speech.stream encoding:    :linear16,
+                         sample_rate: 16000,
+                         language:    "en-US"
+
+  audio_content = File.binread audio_file_path
+  bytes_total   = audio_content.size
+  bytes_sent    = 0
+  chunk_size    = 32000
+
+  # Send chunks of the audio content to the Speech API 1 second at a time
+  while bytes_sent < bytes_total do
+    stream.send audio_content[bytes_sent, chunk_size]
+    bytes_sent += chunk_size
+    sleep 1
+  end
+
+  # Signal the completion of audio content
+  stream.stop
+
+  stream.wait_until_complete!
+
+  results = stream.results
+  results.each do |result|
+    puts "Transcript: #{result.transcript}"
+  end
+# [END speech_streaming]
+end
+
 require "google/cloud/speech"
 
 if __FILE__ == $PROGRAM_NAME
@@ -115,6 +151,8 @@ if __FILE__ == $PROGRAM_NAME
     speech_async_recognize project_id: project_id, audio_file_path: ARGV.first
   when "async_recognize_gcs"
     speech_async_recognize_gcs project_id: project_id, storage_path: ARGV.first
+  when "stream_recognize"
+    speech_streaming_recognize project_id: project_id, audio_file_path: ARGV.first
   else
     puts <<-usage
 Usage: ruby speech_samples.rb <command> [arguments]
@@ -125,6 +163,7 @@ Commands:
   async_recognize     <filename> Creates a job to detect speech in a local audio file, and waits for the job to complete.
   async_recognize_gcs <gcsUri>   Creates a job to detect speech in an audio file located in a Google Cloud Storage bucket, and
                                  waits for the job to complete.
+  stream_recognize    <filename> Detects speech in a local audio file by streaming it to the Speech API.
     usage
   end
 end
