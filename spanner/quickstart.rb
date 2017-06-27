f0c19d608766da162b63c4edd5f553ee1343dff2
Frank Natividad <franknatividad@google.com>
2017-06-26 20:52:03 -0700

    Adding Spanner Quickstart with test

---
+++ spanner/quickstart.rb
@@ -0,0 +1,43 @@
+# Copyright 2017 Google, Inc
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# [START spanner_quickstart]
+# Imports the Google Cloud client library
+require "google/cloud/spanner"
+
+# Your Google Cloud Platform project ID
+project_id = "YOUR_PROJECT_ID"
+
+# Instantiates a client
+spanner = Google::Cloud::Spanner.new project: project_id
+
+# Your Cloud Spanner instance ID
+instance_id = "my-instance";
+
+# Your Cloud Spanner database ID
+database_id = "my-database";
+
+# Gets a reference to a Cloud Spanner instance database
+database_client = spanner.client instance_id, database_id
+
+# Execute a simple SQL statement
+database_client.transaction do |transaction|
+  results = transaction.execute "SELECT 1"
+
+  results.rows.each do |row|
+    puts row
+  end
+end
+# [END spanner_quickstart]
+
