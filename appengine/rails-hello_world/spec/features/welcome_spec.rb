70f8772a3c310295348be2e396aec6739d6f20f8
Frank Natividad <franknatividad@google.com>
2017-07-05 16:44:04 -0700

    Adding e2e tests for Hello World Rails App Engine app

---
+++ appengine/rails-hello_world/spec/features/welcome_spec.rb
@@ -0,0 +1,28 @@
+# Copyright 2017, Google, Inc.
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+require "rails_helper"
+
+RSpec.feature "Hello World" do
+  before :all do
+    skip "End-to-end tets skipped" unless E2E.run?
+
+    @url = E2E.url
+  end
+
+  scenario "should be shown" do
+    visit @url
+
+    expect(page).to have_content "This is a home page for a new Rails App on Google Cloud Platform!"
+  end
+end
---
+++ appengine/rails-hello_world/spec/rails_helper.rb
@@ -0,0 +1,19 @@
+# This file is copied to spec/ when you run 'rails generate rspec:install'
+ENV['RAILS_ENV'] ||= 'test'
+require File.expand_path('../../config/environment', __FILE__)
+# Prevent database truncation if the environment is production
+abort("The Rails environment is running in production mode!") if Rails.env.production?
+require 'spec_helper'
+require 'rspec/rails'
+require 'capybara/rspec'
+require File.expand_path "../../../../spec/e2e", __FILE__
+
+RSpec.configure do |config|
+  config.infer_spec_type_from_file_location!
+
+  # Filter lines from Rails gems in backtraces.
+  config.filter_rails_from_backtrace!
+end
+
+
+
