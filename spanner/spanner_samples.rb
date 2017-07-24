c243b66af19d17645ddba8c169e503eb158b6490
remi Taylor <remily@google.com>
2017-07-24 16:04:24 -0700

    [Spanner] Align { curly braces } for literal hashes

--- spanner/spanner_samples.rb
+++ spanner/spanner_samples.rb
@@ -61,17 +61,17 @@ def insert_data project_id:, instance_id:, database_id:
   client.commit do |c|
     c.insert "Singers", [
       { SingerId: 1, FirstName: "Marc",     LastName: "Richards" },
-      { SingerId: 2, FirstName: "Catalina", LastName: "Smith" },
-      { SingerId: 3, FirstName: "Alice",    LastName: "Trentor" },
-      { SingerId: 4, FirstName: "Lea",      LastName: "Martin" },
-      { SingerId: 5, FirstName: "David",    LastName: "Lomond" }
+      { SingerId: 2, FirstName: "Catalina", LastName: "Smith"    },
+      { SingerId: 3, FirstName: "Alice",    LastName: "Trentor"  },
+      { SingerId: 4, FirstName: "Lea",      LastName: "Martin"   },
+      { SingerId: 5, FirstName: "David",    LastName: "Lomond"   }
     ]
     c.insert "Albums", [
-      { SingerId: 1, AlbumId: 1, AlbumTitle: "Go, Go, Go" },
-      { SingerId: 1, AlbumId: 2, AlbumTitle: "Total Junk" },
-      { SingerId: 2, AlbumId: 1, AlbumTitle: "Green" },
+      { SingerId: 1, AlbumId: 1, AlbumTitle: "Go, Go, Go"              },
+      { SingerId: 1, AlbumId: 2, AlbumTitle: "Total Junk"              },
+      { SingerId: 2, AlbumId: 1, AlbumTitle: "Green"                   },
       { SingerId: 2, AlbumId: 2, AlbumTitle: "Forever Hold your Peace" },
-      { SingerId: 2, AlbumId: 3, AlbumTitle: "Terrified" }
+      { SingerId: 2, AlbumId: 3, AlbumTitle: "Terrified"               }
     ]
   end
 
