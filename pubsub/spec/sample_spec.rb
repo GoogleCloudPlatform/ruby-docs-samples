07b92a463250156c06c841c4110afaa88102507f
Frank Natividad <franknatividad@google.com>
2017-10-19 17:03:41 -0700

    Update Gemfile.lock and fix updated method parameters in tests

--- pubsub/spec/sample_spec.rb
+++ pubsub/spec/sample_spec.rb
@@ -85,11 +85,8 @@ describe "Pub/Sub sample" do
 
   it "deletes subscription" do
     topic = @pubsub.create_topic @topic_name
-    @pubsub.create_subscription(
-      @topic_name,
-      @subscription_name,
-      autocreate: true
-    )
+    topic.subscribe @subscription_name
+
     expect(topic.subscription @subscription_name).not_to be nil
 
     expect { delete_subscription }.to output(
@@ -137,11 +134,9 @@ describe "Pub/Sub sample" do
   it "pulls a message" do
     message = "Test Message"
 
-    @pubsub.create_subscription(
-      @topic_name,
-      @subscription_name,
-      autocreate: true
-    )
+    topic = @pubsub.create_topic @topic_name
+    subscription = topic.subscribe @subscription_name
+
     @pubsub.topic(@topic_name).publish message
 
     expect_with_retry do
@@ -150,7 +145,7 @@ describe "Pub/Sub sample" do
   end
 
   it "lists topics" do
-    @pubsub.topic @topic_name, autocreate: true
+    @pubsub.create_topic @topic_name
 
     expect_with_retry do
       expect { list_topics }.to output(/#{@topic_name}/).to_stdout
@@ -158,11 +153,8 @@ describe "Pub/Sub sample" do
   end
 
   it "lists subscriptions" do
-    @pubsub.create_subscription(
-      @topic_name,
-      @subscription_name,
-      autocreate: true
-    )
+    topic = @pubsub.create_topic @topic_name
+    subscription = topic.subscribe @subscription_name
 
     expect_with_retry do
       expect { list_subscriptions }.to output(/#{@subscription_name}/).to_stdout
@@ -176,11 +168,8 @@ describe "Pub/Sub sample" do
   end
 
   it "gets subscription policy" do
-    @pubsub.create_subscription(
-      @topic_name,
-      @subscription_name,
-      autocreate: true
-    )
+    topic = @pubsub.create_topic @topic_name
+    subscription = topic.subscribe @subscription_name
 
     expect { get_subscription_policy }.to output(/{}/).to_stdout
   end
@@ -203,11 +192,8 @@ describe "Pub/Sub sample" do
   end
 
   it "sets subscription policy" do
-    @pubsub.create_subscription(
-      @topic_name,
-      @subscription_name,
-      autocreate: true
-    )
+    topic = @pubsub.create_topic @topic_name
+    subscription = topic.subscribe @subscription_name
 
     expect_any_instance_of(Google::Cloud::Pubsub::Policy).to \
       receive(:add).with(
@@ -229,11 +215,9 @@ describe "Pub/Sub sample" do
   end
 
   it "tests subscription permissions" do
-    @pubsub.create_subscription(
-      @topic_name,
-      @subscription_name,
-      autocreate: true
-    )
+    topic = @pubsub.create_topic @topic_name
+    subscription = topic.subscribe @subscription_name
+
     expect { test_subscription_permissions }.to output(/true\ntrue/).to_stdout
   end
 end
