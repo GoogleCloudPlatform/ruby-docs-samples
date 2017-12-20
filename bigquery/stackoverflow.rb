442aeccb0be203ebce38a6ebb90073d5861d1786
Tim Swast <swast@google.com>
2017-12-20 14:51:48 -0800

    Align =

--- bigquery/stackoverflow.rb
+++ bigquery/stackoverflow.rb
@@ -26,12 +26,12 @@ bigquery = Google::Cloud::Bigquery.new
 # [END bigquery_simple_app_client]
 
 # [START bigquery_simple_app_query]
-sql = "SELECT " +
-      "CONCAT('https://stackoverflow.com/questions/', " +
-      "       CAST(id as STRING)) as url, view_count " +
-      "FROM `bigquery-public-data.stackoverflow.posts_questions` " +
-      "WHERE tags like '%google-bigquery%' " +
-      "ORDER BY view_count DESC LIMIT 10"
+sql     = "SELECT " +
+          "CONCAT('https://stackoverflow.com/questions/', " +
+          "       CAST(id as STRING)) as url, view_count " +
+          "FROM `bigquery-public-data.stackoverflow.posts_questions` " +
+          "WHERE tags like '%google-bigquery%' " +
+          "ORDER BY view_count DESC LIMIT 10"
 results = bigquery.query sql
 # [END bigquery_simple_app_query]
 
