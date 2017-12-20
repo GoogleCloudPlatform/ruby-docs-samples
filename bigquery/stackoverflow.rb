092dca90554e76201b139a57e5bacf2522ed243b
Tim Swast <swast@google.com>
2017-12-20 14:28:24 -0800

    Bump view_count up a line.

--- bigquery/stackoverflow.rb
+++ bigquery/stackoverflow.rb
@@ -28,8 +28,7 @@ bigquery = Google::Cloud::Bigquery.new
 # [START bigquery_simple_app_query]
 sql = "SELECT " +
       "CONCAT('https://stackoverflow.com/questions/', " +
-      "       CAST(id as STRING)) as url, " +
-      "view_count " +
+      "       CAST(id as STRING)) as url, view_count " +
       "FROM `bigquery-public-data.stackoverflow.posts_questions` " +
       "WHERE tags like '%google-bigquery%' " +
       "ORDER BY view_count DESC LIMIT 10"
