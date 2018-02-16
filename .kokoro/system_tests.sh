c43c27cb305b387004197ed1318d9e5f77cc08ca
Frank Natividad <franknatividad@google.com>
2018-02-16 15:24:59 -0800

    Update system_tests.sh to run storage quickstarts

--- .kokoro/system_tests.sh
+++ .kokoro/system_tests.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
 
-echo "hello ruby"
+source $KOKORO_GFILE_DIR/secrets.sh
+
+export GOOGLE_APPLICATION_CREDENTIALS=$KOKORO_GFILE_DIR/client_secret_978467553869-f5a190d93kfbv0req8ejnpkj41quuncd.apps.googleusercontent.com.json
+
+cd github/ruby-docs-samples/storage
+
+gem install bundle
+bundle install
+bundle exec rspec spec/quickstart_spec.rb
+
