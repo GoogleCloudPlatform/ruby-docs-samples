9b8e494d7c6bff4cfcd132a54f753619cbcb6db2
Frank Natividad <franknatividad@google.com>
2018-02-16 16:07:29 -0800

    Add additional output to system_tests.sh

--- .kokoro/system_tests.sh
+++ .kokoro/system_tests.sh
@@ -5,6 +5,9 @@ source $KOKORO_GFILE_DIR/secrets.sh
 export GOOGLE_APPLICATION_CREDENTIALS=$KOKORO_GFILE_DIR/client_secret_978467553869-f5a190d93kfbv0req8ejnpkj41quuncd.apps.googleusercontent.com.json
 
 cd github/ruby-docs-samples/storage
+echo $GOOGLE_APPLICATION_CREDENTIALS
+ls -l $GOOGLE_APPLICATION_CREDENTIALS
+ls -l $KOKORO_GFILE_DIR
 
 gem install bundle
 bundle install
