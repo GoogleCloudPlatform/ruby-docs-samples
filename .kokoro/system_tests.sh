0d96e9ba460277fa4d5669c167ab3a9a468dc2b8
Frank Natividad <franknatividad@google.com>
2018-02-16 16:32:08 -0800

    Update system_tests.sh

--- .kokoro/system_tests.sh
+++ .kokoro/system_tests.sh
@@ -2,8 +2,6 @@
 
 source $KOKORO_GFILE_DIR/secrets.sh
 
-export GOOGLE_APPLICATION_CREDENTIALS=$KOKORO_GFILE_DIR/cloud-samples-tests-ruby-35fe8d65344c.json
-
 cd github/ruby-docs-samples/storage
 echo $GOOGLE_APPLICATION_CREDENTIALS
 ls -l $GOOGLE_APPLICATION_CREDENTIALS
