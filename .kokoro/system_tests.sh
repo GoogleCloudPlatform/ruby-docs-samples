4b2565edcc7aea21890430b2267525e13a71df26
Frank Natividad <franknatividad@google.com>
2018-03-02 16:52:16 -0800

    Remove cloudsql proxy for now.

--- .kokoro/system_tests.sh
+++ .kokoro/system_tests.sh
@@ -2,14 +2,5 @@
 
 source $KOKORO_GFILE_DIR/secrets.sh
 
-sudo apt-get update
-sudo apt-get install realpath wget
-wget https://dl.google.com/cloudsql/cloud_sql_proxy.linux.amd64
-
-# Unsure about the following section because of Ruby image
-mv cloud_sql_proxy.linux.amd64 $HOME/cloud_sql_proxy
-chmod +x $HOME/cloud_sql_proxy
-sudo mkdir /cloudsql && sudo chmod 0777 /cloudsql
-
 cd github/ruby-docs-samples/
 ./spec/kokoro-run-all.sh
