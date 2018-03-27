e1e35046631133af457580fa0b5e0a4a93c2ecf2
Frank Natividad <franknatividad@google.com>
2018-03-27 12:58:59 -0700

    Add retries to kms

--- kms/spec/quickstart_spec.rb
+++ kms/spec/quickstart_spec.rb
@@ -13,7 +13,19 @@
 # limitations under the License.
 
 require "rspec"
+require "rspec/retry"
 require "google/apis/cloudkms_v1"
+
+RSpec.configure do |config|
+  # show retry status in spec process
+  config.verbose_retry = true
+  # show exception that triggers a retry if verbose_retry is set to true
+  config.display_try_failure_messages = true
+
+  # set retry count and retry sleep interval to 10 seconds
+  config.default_retry_count = 5
+  config.default_sleep_interval = 10
+end
 
 describe "Key Management Service Quickstart" do
   Cloudkms = Google::Apis::CloudkmsV1
