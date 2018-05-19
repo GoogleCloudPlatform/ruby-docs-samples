18348ac8a7657cc5c7cb4b67d675990b7270ad18
Ryan Matsumoto <ryanmats@google.com>
2018-05-18 17:56:28 -0700

    Query Data samples

---
+++ firestore/query_data.rb
@@ -0,0 +1,172 @@
+# Copyright 2018 Google, Inc
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+require "google/cloud/firestore"
+
+def query_create_examples project_id:
+  # project_id = "Your Google Cloud Project ID"
+  firestore = Google::Cloud::Firestore.new(project_id: project_id)
+  # [START fs_query_create_examples]
+  cities_ref = firestore.col "cities"
+  cities_ref.doc("SF").set({
+    name: "San Francisco",
+    state: "CA",
+    country: "USA",
+    capital: false,
+    population: 860000
+  })
+  cities_ref.doc("LA").set({
+    name: "Los Angeles",
+    state: "CA",
+    country: "USA",
+    capital: false,
+    population: 3900000
+  })
+  cities_ref.doc("DC").set({
+    name: "Washington D.C.",
+    state: nil,
+    country: "USA",
+    capital: true,
+    population: 680000
+  })
+  cities_ref.doc("TOK").set({
+    name: "Tokyo",
+    state: nil,
+    country: "Japan",
+    capital: true,
+    population: 9000000
+  })
+  cities_ref.doc("BJ").set({
+    name: "Beijing",
+    state: nil,
+    country: "China",
+    capital: true,
+    population: 21500000
+  })
+  # [END fs_query_create_examples]
+  puts "Added example cities data to the cities collection."
+end
+
+def create_query_state project_id:
+  # project_id = "Your Google Cloud Project ID"
+  firestore = Google::Cloud::Firestore.new(project_id: project_id)
+  # [START fs_create_query_state]
+  cities_ref = firestore.col "cities"
+  query = cities_ref.where("state", "=", "CA")
+  query.get do |city|
+    puts "Document #{city.document_id} returned by query state=CA."
+  end
+  # [END fs_create_query_state]
+end
+
+def create_query_capital project_id:
+  # project_id = "Your Google Cloud Project ID"
+  firestore = Google::Cloud::Firestore.new(project_id: project_id)
+  # [START fs_create_query_capital]
+  cities_ref = firestore.col "cities"
+  query = cities_ref.where("capital", "=", true)
+  query.get do |city|
+    puts "Document #{city.document_id} returned by query capital=true."
+  end
+  # [END fs_create_query_capital]
+end
+
+def simple_queries project_id:
+  # project_id = "Your Google Cloud Project ID"
+  firestore = Google::Cloud::Firestore.new(project_id: project_id)
+  cities_ref = firestore.col "cities"
+  # [START fs_simple_queries]
+  state_query = cities_ref.where("state", "=", "CA")
+  population_query = cities_ref.where("population", ">", 1000000)
+  name_query = cities_ref.where("name", ">=", "San Francisco")
+  # [END fs_simple_queries]
+  state_query.get do |city|
+    puts "Document #{city.document_id} returned by query state=CA."
+  end
+  population_query.get do |city|
+    puts "Document #{city.document_id} returned by query population>1000000."
+  end
+  name_query.get do |city|
+    puts "Document #{city.document_id} returned by query name>=San Francisco."
+  end
+end
+
+def chained_query project_id:
+  # project_id = "Your Google Cloud Project ID"
+  firestore = Google::Cloud::Firestore.new(project_id: project_id)
+  cities_ref = firestore.col "cities"
+  # [START fs_chained_query]
+  chained_query = cities_ref.where("state", "=", "CA").where("name", "=", "San Francisco")
+  # [END fs_chained_query]
+  chained_query.get do |city|
+    puts "Document #{city.document_id} returned by query state=CA and name=San Francisco."
+  end
+end
+
+def composite_index_chained_query project_id:
+  # project_id = "Your Google Cloud Project ID"
+  firestore = Google::Cloud::Firestore.new(project_id: project_id)
+  cities_ref = firestore.col "cities"
+  # [START fs_composite_index_chained_query]
+  chained_query = cities_ref.where("state", "=", "CA").where("population", "<", 1000000)
+  # [END fs_composite_index_chained_query]
+  chained_query.get do |city|
+    puts "Document #{city.document_id} returned by query state=CA and population<1000000."
+  end
+end
+
+def range_query project_id:
+  # project_id = "Your Google Cloud Project ID"
+  firestore = Google::Cloud::Firestore.new(project_id: project_id)
+  cities_ref = firestore.col "cities"
+  # [START fs_range_query]
+  range_query = cities_ref.where("state", ">=", "CA").where("state", "<=", "IN")
+  # [END fs_range_query]
+  range_query.get do |city|
+    puts "Document #{city.document_id} returned by query CA<=state<=IN."
+  end
+end
+
+def invalid_range_query project_id:
+  # project_id = "Your Google Cloud Project ID"
+  firestore = Google::Cloud::Firestore.new(project_id: project_id)
+  cities_ref = firestore.col "cities"
+  # [START fs_invalid_range_query]
+  invalid_range_query = cities_ref.where("state", ">=", "CA").where("population", ">", 1000000)
+  # [END fs_invalid_range_query]
+end
+
+
+if __FILE__ == $0
+  case ARGV.shift
+  when "query_create_examples"
+    query_create_examples project_id: ENV["GOOGLE_CLOUD_PROJECT"]
+  when "create_query_state"
+    create_query_state project_id: ENV["GOOGLE_CLOUD_PROJECT"]
+  when "create_query_capital"
+    create_query_capital project_id: ENV["GOOGLE_CLOUD_PROJECT"]
+  when "simple_queries"
+    simple_queries project_id: ENV["GOOGLE_CLOUD_PROJECT"]
+  when "chained_query"
+    chained_query project_id: ENV["GOOGLE_CLOUD_PROJECT"]
+  when "composite_index_chained_query"
+    composite_index_chained_query project_id: ENV["GOOGLE_CLOUD_PROJECT"]
+  when "range_query"
+    range_query project_id: ENV["GOOGLE_CLOUD_PROJECT"]
+  when "invalid_range_query"
+    invalid_range_query project_id: ENV["GOOGLE_CLOUD_PROJECT"]
+  else
+    puts "Command not found!"
+  end
+end
