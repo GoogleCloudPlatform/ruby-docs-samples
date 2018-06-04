7518f300f0a0956665855bab617effc065732649
Tyler Bui-Palsulich <tbp@google.com>
2018-06-04 15:08:52 -0400

    kokoro: use project pool

--- .kokoro/system_tests.sh
+++ .kokoro/system_tests.sh
@@ -2,5 +2,15 @@
 
 source $KOKORO_GFILE_DIR/secrets.sh
 
+curl https://storage.googleapis.com/gimme-proj/linux_amd64/gimmeproj > /bin/gimmeproj && chmod +x /bin/gimmeproj;
+gimmeproj version;
+export GOOGLE_CLOUD_PROJECT=$(gimmeproj -project cloud-samples-ruby-test-kokoro lease 30m);
+if [ -z "$GOOGLE_CLOUD_PROJECT" ]; then
+  echo "Lease failed."
+  exit 1
+fi
+echo "Running tests in project $GOOGLE_CLOUD_PROJECT";
+trap "gimmeproj -project cloud-samples-ruby-test-kokoro done $GOOGLE_CLOUD_PROJECT" EXIT
+
 cd github/ruby-docs-samples/
 ./spec/kokoro-run-all.sh
