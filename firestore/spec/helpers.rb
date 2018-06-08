bcfb3634bc185a12c6bf35b905c1ec2dbc1b2502
Ryan Matsumoto <ryanmats@google.com>
2018-06-08 14:26:07 -0700

    Adding helpers.rb changes

--- firestore/spec/helpers.rb
+++ firestore/spec/helpers.rb
@@ -1,17 +1,7 @@
 require "google/cloud/firestore"
 
-def delete_collection collection_name:
-  firestore = Google::Cloud::Firestore.new project_id: ENV["FIRESTORE_PROJECT_ID"]
-  cities_ref = firestore.col collection_name
-  query = cities_ref
-  query.get do |document_snapshot|
-    document_ref = document_snapshot.ref
-    document_ref.delete
-  end
-end
-
-def delete_collection_test collection_name:
-  firestore = Google::Cloud::Firestore.new project_id: ENV["GOOGLE_CLOUD_PROJECT"]
+def delete_collection_test collection_name:, project_id:
+  firestore = Google::Cloud::Firestore.new project_id: project_id
   cities_ref = firestore.col collection_name
   query = cities_ref
   query.get do |document_snapshot|
