e7590661706fd6e80c5bdad6b633fd9680e58b95
Ryan Matsumoto <ryanmats@google.com>
2018-06-08 19:32:21 -0700

    Add retries to tests

--- firestore/spec/paginate_data_spec.rb
+++ firestore/spec/paginate_data_spec.rb
@@ -2,13 +2,24 @@ require_relative "../get_data.rb"
 require_relative "../paginate_data.rb"
 require_relative "helpers.rb"
 require "rspec"
+require "rspec/retry"
+
+RSpec.configure do |config|
+  # show retry status in spec process
+  config.verbose_retry = true
+  # show exception that triggers a retry if verbose_retry is set to true
+  config.display_try_failure_messages = true
+
+  # set retry count and retry sleep interval to 5 seconds
+  config.default_retry_count = 5
+  config.default_sleep_interval = 5
+end
 
 describe "Google Cloud Firestore API samples - Paginate Data" do
 
   before do
     @firestore_project = ENV["FIRESTORE_PROJECT_ID"]
     retrieve_create_examples project_id: @firestore_project
-    sleep(1)
   end
 
   after do
