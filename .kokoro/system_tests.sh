f3a92601995e64243191fac023f279ac78502adb
Tyler Bui-Palsulich <tbp@google.com>
2018-06-11 15:53:51 -0400

    kokoro: set alt bucket variable

--- .kokoro/system_tests.sh
+++ .kokoro/system_tests.sh
@@ -13,6 +13,7 @@ echo "Running tests in project $GOOGLE_CLOUD_PROJECT";
 trap "gimmeproj -project cloud-samples-ruby-test-kokoro done $GOOGLE_CLOUD_PROJECT" EXIT
 
 export GOOGLE_CLOUD_STORAGE_BUCKET="$GOOGLE_CLOUD_PROJECT-$GOOGLE_CLOUD_STORAGE_BUCKET"
+export ALTERNATE_GOOGLE_CLOUD_STORAGE_BUCKET="$GOOGLE_CLOUD_STORAGE_BUCKET-alt"
 
 cd github/ruby-docs-samples/
 ./spec/kokoro-run-all.sh
