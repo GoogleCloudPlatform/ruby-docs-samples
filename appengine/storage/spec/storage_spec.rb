de865ff318d4ee641fade275c7b47caef408365a
Tyler Bui-Palsulich <26876514+tbpg@users.noreply.github.com>
2018-07-26 12:04:16 -0400

    kokoro: enable appengine/storage tests (#312)

--- appengine/storage/spec/storage_spec.rb
+++ appengine/storage/spec/storage_spec.rb
@@ -12,37 +12,26 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-require File.expand_path("../../../../spec/e2e", __FILE__)
+require_relative "../app.rb"
 require "rspec"
 require "capybara/rspec"
 require "capybara/poltergeist"
 
-Capybara.current_driver = :poltergeist
-
-describe "Cloud Storage on Google App Engine", type: :feature do
-  before :all do
-    skip "End-to-end tests skipped" unless E2E.run?
-
-    app_yaml = File.expand_path("../../app.yaml", __FILE__)
-    configuration = File.read(app_yaml)
-                        .sub("<your-bucket-name>", ENV["GOOGLE_CLOUD_STORAGE_BUCKET"])
-    File.write(app_yaml, configuration)
-
-    @url = E2E.url
+describe "Cloud Storage", type: :feature do
+  before do
+    Capybara.current_driver = :poltergeist
   end
-
   it "can upload and get public URL of uploaded file" do
+    Capybara.app = Sinatra::Application
     file_path = File.expand_path("../ruby-storage-test-upload.txt", __FILE__)
 
-    visit @url
+    visit "/"
     attach_file "file", file_path
     click_button "Upload"
 
-    uploaded_file_public_url = find("body").text
+    uploaded_file_public_url = page.find("body").text
 
     visit uploaded_file_public_url
-    expect(find("body").text).to eq(
-      "This is the content of the test-upload.txt file"
-    )
+    expect(page).to have_content "This is the content of the test-upload.txt file"
   end
 end
