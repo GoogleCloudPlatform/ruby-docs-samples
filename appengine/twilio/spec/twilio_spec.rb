3c1b97fbbfdb5367e56e1e6fae327eff0cb7a268
Tyler Bui-Palsulich <26876514+tbpg@users.noreply.github.com>
2018-07-27 09:42:33 -0400

    kokoro: enable appengine/twilio tests (#317)

--- appengine/twilio/spec/twilio_spec.rb
+++ appengine/twilio/spec/twilio_spec.rb
@@ -12,49 +12,47 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-require File.expand_path("../../../../spec/e2e", __FILE__)
+require_relative "../app.rb"
 require "rspec"
+require "rack/test"
 require "rest-client"
 
-describe "Twilio on Google App Engine", type: :feature do
-  before :all do
-    skip "End-to-end tests skipped" unless E2E.run?
+describe "Twilio", type: :feature do
+  include Rack::Test::Methods
 
-    @url = E2E.url
+  def app
+    Sinatra::Application
   end
 
   it "can send SMS" do
     # Server will raise Twilio::REST::RequestError because Twilio account
     # information is fake
-    expect {
-      RestClient.get "#{@url}/sms/send", to: "+15551112222"
-    }.to raise_error(
-      RestClient::InternalServerError
-    )
+      get "/sms/send", to: "+15551112222"
+      expect(last_response.status).to eq 500
   end
 
   it "can receive SMS" do
-    response = RestClient.post "#{@url}/sms/receive", From: "+15551112222",
-                                                      Body: "Hello"
+    post "/sms/receive", From: "+15551112222",
+                         Body: "Hello"
 
-    expect(response.code).to eq 200
-    expect(response.headers[:content_type]).to eq(
+    expect(last_response.ok?).to eq(true)
+    expect(last_response.headers["Content-Type"]).to eq(
       "application/xml;charset=utf-8"
     )
-    expect(response.body).to include(
+    expect(last_response.body).to include(
       "<Message>Hello +15551112222, you said Hello</Message>"
     )
   end
 
   it "can receive call" do
-    response = RestClient.post "#{@url}/call/receive", From: "+15551112222"
+    post "/call/receive", From: "+15551112222"
 
-    expect(response.code).to eq 200
-    expect(response.headers[:content_type]).to eq(
+    expect(last_response.ok?).to eq(true)
+    expect(last_response.headers["Content-Type"]).to eq(
       "application/xml;charset=utf-8"
     )
-    expect(response.body).to include(
-      "<Response><Say>Hello from Twilio!</Say></Response>"
+    expect(last_response.body).to include(
+      "<Say>Hello from Twilio!</Say>"
     )
   end
 end
