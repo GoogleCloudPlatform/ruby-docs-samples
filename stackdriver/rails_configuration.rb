a011b6fe114850cbb14e85219dc83f4e7156d071
TheRoyalTnetennba <paye@google.com>
2018-10-17 14:41:34 -0700

    add additional step for running in development mode

--- stackdriver/rails_configuration.rb
+++ stackdriver/rails_configuration.rb
@@ -16,8 +16,8 @@
 # Add this to config/environments/*.rb
 Rails.application.configure do |config|
   # Stackdriver Shared parameters
-  config.google_cloud.project_id = "YOUR-PROJECT-ID"
-  config.google_cloud.keyfile    = "/path/to/service-account.json"
+  config.google_cloud.project_id = 'YOUR-PROJECT-ID'
+  config.google_cloud.keyfile    = '/path/to/service-account.json'
 end
 # [END logging_rails_shared_configure]
 
@@ -25,34 +25,62 @@ end
 # Add this to config/environments/*.rb
 Rails.application.configure do |config|
   # Stackdriver Debugger specific parameters
-  config.google_cloud.debugger.project_id = "YOUR-PROJECT-ID"
-  config.google_cloud.debugger.keyfile    = "/path/to/service-account.json"
+  config.google_cloud.debugger.project_id = 'YOUR-PROJECT-ID'
+  config.google_cloud.debugger.keyfile    = '/path/to/service-account.json'
 end
 # [END debugger_configure]
+
+# [START debugger_configure_development]
+# Add this to config/environments/development.rb
+Rails.application.configure do |config|
+  config.google_cloud.use_debugger = true
+end
+# [END debugger_configure_development]
 
 # [START trace_configure]
 # Add this to config/environments/*.rb
 Rails.application.configure do |config|
   # Stackdriver Trace specific parameters
-  config.google_cloud.trace.project_id = "YOUR-PROJECT-ID"
-  config.google_cloud.trace.keyfile    = "/path/to/service-account.json"
+  config.google_cloud.trace.project_id = 'YOUR-PROJECT-ID'
+  config.google_cloud.trace.keyfile    = '/path/to/service-account.json'
 end
 # [END trace_configure]
+
+# [START trace_configure_development]
+# Add this to config/environments/development.rb
+Rails.application.configure do |config|
+  config.google_cloud.use_trace = true
+end
+# [END trace_configure_development]
 
 # [START error_reporting_configure]
 # Add this to config/environments/*.rb
 Rails.application.configure do |config|
   # Stackdriver Error Reporting specific parameters
-  config.google_cloud.error_reporting.project_id = "YOUR-PROJECT-ID"
-  config.google_cloud.error_reporting.keyfile    = "/path/to/service-account.json"
+  config.google_cloud.error_reporting.project_id = 'YOUR-PROJECT-ID'
+  config.google_cloud.error_reporting.keyfile    = '/path/to/service-account.json'
 end
 # [END error_reporting_configure]
+
+# [START error_reporting_configure_development]
+# Add this to config/environments/development.rb
+Rails.application.configure do |config|
+  config.google_cloud.use_error_reporting = true
+end
+# [END error_reporting_configure_development]
 
 # [START logging_rails_client_configure]
 # Add this to config/environments/*.rb
 Rails.application.configure do |config|
   # Stackdriver Logging specific parameters
-  config.google_cloud.logging.project_id = "YOUR-PROJECT-ID"
-  config.google_cloud.logging.keyfile    = "/path/to/service-account.json"
+  config.google_cloud.logging.project_id = 'YOUR-PROJECT-ID'
+  config.google_cloud.logging.keyfile    = '/path/to/service-account.json'
 end
 # [END logging_rails_client_configure]
+
+# [START logging_configure_development]
+# Add this to config/environments/development.rb
+Rails.application.configure do |config|
+  config.google_cloud.use_logging = true
+end
+# [END logging_configure_development]
