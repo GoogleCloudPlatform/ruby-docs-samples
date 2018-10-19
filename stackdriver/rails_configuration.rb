e579b8532a479e6ca8b7280bcb6e72e15bb02690
Graham Paye <gpaye8@gmail.com>
2018-10-19 09:58:15 -0700

    use double quotes

--- stackdriver/rails_configuration.rb
+++ stackdriver/rails_configuration.rb
@@ -16,8 +16,8 @@
 # Add this to config/environments/*.rb
 Rails.application.configure do |config|
   # Stackdriver Shared parameters
-  config.google_cloud.project_id = 'YOUR-PROJECT-ID'
-  config.google_cloud.keyfile    = '/path/to/service-account.json'
+  config.google_cloud.project_id = "YOUR-PROJECT-ID"
+  config.google_cloud.keyfile    = "/path/to/service-account.json"
 end
 # [END logging_rails_shared_configure]
 
@@ -25,8 +25,8 @@ end
 # Add this to config/environments/*.rb
 Rails.application.configure do |config|
   # Stackdriver Debugger specific parameters
-  config.google_cloud.debugger.project_id = 'YOUR-PROJECT-ID'
-  config.google_cloud.debugger.keyfile    = '/path/to/service-account.json'
+  config.google_cloud.debugger.project_id = "YOUR-PROJECT-ID"
+  config.google_cloud.debugger.keyfile    = "/path/to/service-account.json"
 end
 # [END debugger_configure]
 
@@ -41,8 +41,8 @@ end
 # Add this to config/environments/*.rb
 Rails.application.configure do |config|
   # Stackdriver Trace specific parameters
-  config.google_cloud.trace.project_id = 'YOUR-PROJECT-ID'
-  config.google_cloud.trace.keyfile    = '/path/to/service-account.json'
+  config.google_cloud.trace.project_id = "YOUR-PROJECT-ID"
+  config.google_cloud.trace.keyfile    = "/path/to/service-account.json"
 end
 # [END trace_configure]
 
@@ -57,8 +57,8 @@ end
 # Add this to config/environments/*.rb
 Rails.application.configure do |config|
   # Stackdriver Error Reporting specific parameters
-  config.google_cloud.error_reporting.project_id = 'YOUR-PROJECT-ID'
-  config.google_cloud.error_reporting.keyfile    = '/path/to/service-account.json'
+  config.google_cloud.error_reporting.project_id = "YOUR-PROJECT-ID"
+  config.google_cloud.error_reporting.keyfile    = "/path/to/service-account.json"
 end
 # [END error_reporting_configure]
 
@@ -73,8 +73,8 @@ end
 # Add this to config/environments/*.rb
 Rails.application.configure do |config|
   # Stackdriver Logging specific parameters
-  config.google_cloud.logging.project_id = 'YOUR-PROJECT-ID'
-  config.google_cloud.logging.keyfile    = '/path/to/service-account.json'
+  config.google_cloud.logging.project_id = "YOUR-PROJECT-ID"
+  config.google_cloud.logging.keyfile    = "/path/to/service-account.json"
 end
 # [END logging_rails_client_configure]
 
