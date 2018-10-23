524649b022c4d1f2ca6ba12aaeac5f6e748c9c86
Alix Hamilton <ajhamilton@google.com>
2018-10-23 11:14:49 -0700

    add newline between copyright and code

--- dlp/dlp_inspect_text_file.rb
+++ dlp/dlp_inspect_text_file.rb
@@ -11,6 +11,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+
 # [START dlp_inspect_text_file]
 require "google/cloud/dlp"
 
