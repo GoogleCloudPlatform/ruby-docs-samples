7d964d53682ba43c44b4f253113f4013baedffae
TheRoyalTnetennba <paye@google.com>
2018-11-05 12:29:55 -0800

    fix variable name

--- vision/spec/quickstart_spec.rb
+++ vision/spec/quickstart_spec.rb
@@ -21,7 +21,7 @@ describe "Vision Quickstart" do
     image_annotator = Google::Cloud::Vision::ImageAnnotator.new
     expect(Google::Cloud::Vision::ImageAnnotator).to receive(:new).
                                      with(no_args).
-                                     and_return(vision)
+                                     and_return(image_annotator)
 
     expect {
       load File.expand_path("../quickstart.rb", __dir__)
