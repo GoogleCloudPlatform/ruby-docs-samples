bc9f291519d132a5ffd4fd56876e62df188c3581
averikitsch <akitsch@google.com>
2019-04-10 09:15:51 -0700

    Add freeze and Error back

--- appengine/cloud-tasks/spec/task_spec.rb
+++ appengine/cloud-tasks/spec/task_spec.rb
@@ -24,17 +24,17 @@ describe "CloudTasks", type: :feature do
   before :all do
     GOOGLE_CLOUD_PROJECT = ENV["GOOGLE_CLOUD_PROJECT"]
     location_id          = ENV["LOCATION_ID"] || "us-east1"
-    QUEUE_ID             = ENV["QUEUE_ID"] || "my-appengine-queue"
+    QUEUE_ID             = "my-appengine-queue".freeze
 
     client = Google::Cloud::Tasks.new
     parent = client.queue_path GOOGLE_CLOUD_PROJECT, location_id, QUEUE_ID
 
     begin
       client.get_queue parent
-    rescue
+    rescue StandardError
       location_id = "us-east4"
     end
-    LOCATION_ID = location_id
+    LOCATION_ID = location_id.freeze
   end
 
   def app
