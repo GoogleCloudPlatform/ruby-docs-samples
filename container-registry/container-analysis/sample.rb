959a60edb52f6387e0262e1fd075fa30fd7a2f67
Dan Sanche <sanche@google.com>
2019-05-09 19:17:19 -0700

    added container analysis samples

---
+++ container-registry/container-analysis/sample.rb
@@ -0,0 +1,320 @@
+# Copyright 2019 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+
+$LOAD_PATH.unshift('./google-cloud-containeranalysis/lib')
+
+def create_note note_id:, project_id:
+  # [START containeranalysis_create_note]
+  # note_id    = "A user-specified identifier for the note"
+  # project_id = "Your Google Cloud project ID"
+
+  require "google/cloud/devtools/containeranalysis"
+  containerAnalysis = Google::Cloud::Devtools::Containeranalysis
+
+  # Initialize the client
+  grafeas_v1_beta1_client = containerAnalysis::GrafeasV1Beta1.new(version: :v1beta1)
+
+  formatted_parent = containerAnalysis::V1beta1::GrafeasV1Beta1Client.project_path(project_id)
+  note = {vulnerability: {}}
+  response = grafeas_v1_beta1_client.create_note(formatted_parent, note_id, note)
+  ## [END containeranalysis_create_note]
+  return response
+end
+
+def delete_note note_id:, project_id:
+  # [START containeranalysis_delete_note]
+  # note_id    = "The identifier for the note to delete"
+  # project_id = "The Google Cloud project ID of the note to delete"
+
+  require "google/cloud/devtools/containeranalysis"
+  containerAnalysis = Google::Cloud::Devtools::Containeranalysis
+
+  # Initialize the client
+  grafeas_v1_beta1_client = containerAnalysis::GrafeasV1Beta1.new(version: :v1beta1)
+
+  formatted_parent = containerAnalysis::V1beta1::GrafeasV1Beta1Client.note_path(project_id, note_id)
+  response = grafeas_v1_beta1_client.delete_note(formatted_parent)
+  # [END containeranalysis_delete_note]
+  return response
+end
+
+def create_occurrence resource_url:, note_id:, occurrence_project:, note_project:
+  # [START containeranalysis_create_occurrence]
+  # resource_url       = "The URL of the resource associated with the occurrence, eg. https://gcr.io/project/image@sha256:123"
+  # note_id            = "The identifier of the note associated with the occurrence"
+  # occurrence_project = "The Google Cloud project ID for the new occurrence"
+  # note_project       = "The Google Cloud project ID of the associated note"
+
+  require "google/cloud/devtools/containeranalysis"
+  containerAnalysis = Google::Cloud::Devtools::Containeranalysis
+
+  # Initialize the client
+  grafeas_v1_beta1_client = containerAnalysis::GrafeasV1Beta1.new(version: :v1beta1)
+  formatted_note = containerAnalysis::V1beta1::GrafeasV1Beta1Client.note_path(note_project, note_id)
+  formatted_occurrence_project = containerAnalysis::V1beta1::GrafeasV1Beta1Client.project_path(occurrence_project)
+
+  occurrence = {note_name: formatted_note, vulnerability: {}, resource:{uri: resource_url}}
+
+  response = grafeas_v1_beta1_client.create_occurrence(formatted_occurrence_project, occurrence)
+  # [END containeranalysis_create_occurrence]
+  return response
+end
+
+def delete_occurrence occurrence_id:, project_id:
+  # [START containeranalysis_delete_occurrence]
+  # occurrence_id = "The API-generated identifier associated with the occurrence"
+  # project_id    = "The Google Cloud project ID of the occurrence to delete"
+
+  require "google/cloud/devtools/containeranalysis"
+  containerAnalysis = Google::Cloud::Devtools::Containeranalysis
+
+  # Initialize the client
+  grafeas_v1_beta1_client = containerAnalysis::GrafeasV1Beta1.new(version: :v1beta1)
+
+  formatted_parent = containerAnalysis::V1beta1::GrafeasV1Beta1Client.occurrence_path(project_id, occurrence_id)
+  grafeas_v1_beta1_client.delete_occurrence(formatted_parent)
+  # [END containeranalysis_delete_occurrence]
+end
+
+
+def get_note note_id:, project_id:
+  # [START containeranalysis_get_note]
+  # note_id    = "The identifier for the note to retrieve"
+  # project_id = "The Google Cloud project ID of the note to retrieve"
+
+  require "google/cloud/devtools/containeranalysis"
+  containerAnalysis = Google::Cloud::Devtools::Containeranalysis
+
+  # Initialize the client
+  grafeas_v1_beta1_client = containerAnalysis::GrafeasV1Beta1.new(version: :v1beta1)
+
+  formatted_note = containerAnalysis::V1beta1::GrafeasV1Beta1Client.note_path(project_id, note_id)
+  response = grafeas_v1_beta1_client.get_note(formatted_note)
+  # [END containeranalysis_get_note]
+  return response
+end
+
+def get_occurrence occurrence_id:, project_id:
+  # [START containeranalysis_get_occurrence]
+  # occurrence_id = "The API-generated identifier associated with the occurrence"
+  # project_id    = "The Google Cloud project ID of the occurrence to retrieve"
+
+  require "google/cloud/devtools/containeranalysis"
+  containerAnalysis = Google::Cloud::Devtools::Containeranalysis
+
+  # Initialize the client
+  grafeas_v1_beta1_client = containerAnalysis::GrafeasV1Beta1.new(version: :v1beta1)
+
+  formatted_parent = containerAnalysis::V1beta1::GrafeasV1Beta1Client.occurrence_path(project_id, occurrence_id)
+  response = grafeas_v1_beta1_client.get_occurrence(formatted_parent)
+  # [END containeranalysis_get_occurrence]
+  return response
+end
+
+def get_occurrences_for_image resource_url:, project_id:
+  # [START containeranalysis_occurrences_for_image]
+  # Initialize the client
+  # resource_url = "The URL of the resource associated with the occurrence, eg. https://gcr.io/project/image@sha256:123"
+  # project_id    = "The Google Cloud project ID of the occurrences to retrieve"
+
+  require "google/cloud/devtools/containeranalysis"
+  containerAnalysis = Google::Cloud::Devtools::Containeranalysis
+
+  # Initialize the client
+  grafeas_v1_beta1_client = containerAnalysis::GrafeasV1Beta1.new(version: :v1beta1)
+
+  formatted_parent = containerAnalysis::V1beta1::GrafeasV1Beta1Client.project_path(project_id)
+  filter = "resourceUrl = \"#{resource_url}\""
+  count = 0
+  grafeas_v1_beta1_client.list_occurrences(formatted_parent, filter:filter).each do |occurrence|
+     # Process occurrence here
+     puts occurrence
+     count = count + 1
+  end
+  puts "Found #{count} occurrences"
+  # [END containeranalysis_occurrences_for_image]
+  return count
+end
+
+def get_occurrences_for_note note_id:, project_id:
+  # [START containeranalysis_occurrences_for_note]
+  # note_id    = "The identifier for the note to query"
+  # project_id = "The Google Cloud project ID of the occurrences to retrieve"
+
+  require "google/cloud/devtools/containeranalysis"
+  containerAnalysis = Google::Cloud::Devtools::Containeranalysis
+
+  # Initialize the client
+  grafeas_v1_beta1_client = containerAnalysis::GrafeasV1Beta1.new(version: :v1beta1)
+
+  formatted_note = containerAnalysis::V1beta1::GrafeasV1Beta1Client.note_path(project_id, note_id)
+  count = 0
+  grafeas_v1_beta1_client.list_note_occurrences(formatted_note).each do |occurrence|
+     # Process occurrence here
+     puts occurrence
+     count = count + 1
+  end
+  puts "Found #{count} occurrences"
+  # [END containeranalysis_occurrences_for_image]
+  return count
+end
+
+def get_discovery_info resource_url:, project_id:
+  # [START containeranalysis_discovery_info]
+  # resource_url = "The URL of the resource associated with the occurrence, eg. https://gcr.io/project/image@sha256:123"
+  # project_id   = "The Google Cloud project ID of the occurrences to retrieve"
+
+  require "google/cloud/devtools/containeranalysis"
+  containerAnalysis = Google::Cloud::Devtools::Containeranalysis
+
+  # Initialize the client
+  grafeas_v1_beta1_client = containerAnalysis::GrafeasV1Beta1.new(version: :v1beta1)
+
+  formatted_parent = containerAnalysis::V1beta1::GrafeasV1Beta1Client.project_path(project_id)
+  filter = "kind = \"DISCOVERY\" AND resourceUrl = \"#{resource_url}\""
+  grafeas_v1_beta1_client.list_occurrences(formatted_parent, filter:filter).each do |occurrence|
+     # Process discovery occurrence here
+     puts occurrence
+  end
+  # [END containeranalysis_discovery_info]
+end
+
+def occurrence_pubsub subscription_id:, timeout_seconds:, project_id:
+  # [START containeranalysis_pubsub]
+  # subscription_id = "A user-specified identifier for the new subscription"
+  # timeout_seconds = "The number of seconds to listen for new Pub/Sub messages"
+  # project_id      = "Your Google Cloud project ID"
+
+  require "google/cloud/pubsub"
+
+  pubsub = Google::Cloud::Pubsub.new project: project_id
+  topic = pubsub.topic "container-analysis-occurrences-v1beta1"
+  subscription = topic.subscribe subscription_id
+
+  count = 0
+  subscriber = subscription.listen do |received_message|
+    count += 1
+    # Process incoming occurrence here
+    puts "Message #{count}: #{received_message.data}"
+    received_message.acknowledge!
+  end
+  subscriber.start
+  # Wait for incomming occurrences
+  sleep timeout_seconds
+  subscriber.stop.wait!
+  subscription.delete
+  # Print and return the total number of Pub/Sub messages received
+  puts "Total Messges Received: #{count}"
+  return count
+  # [END containeranalysis_pubsub]
+end
+
+def poll_discovery_finished resource_url:, timeout_seconds:, project_id:
+  # [START containeranalysis_poll_discovery_occurrence_finished]
+  # resource_url    = "The URL of the resource associated with the occurrence, eg. https://gcr.io/project/image@sha256:123"
+  # timeout_seconds = "The number of seconds to wait for the discovery occurrence"
+  # project_id      = "Your Google Cloud project ID"
+
+  require "google/cloud/devtools/containeranalysis"
+  containerAnalysis = Google::Cloud::Devtools::Containeranalysis
+
+  deadline = Time.now + timeout_seconds
+
+  # Initialize the client
+  grafeas_v1_beta1_client = containerAnalysis::GrafeasV1Beta1.new(version: :v1beta1)
+  formatted_parent = containerAnalysis::V1beta1::GrafeasV1Beta1Client.project_path(project_id)
+
+  # Find the discovery occurrence using a filter string
+  discovery_occurrence = nil
+  while discovery_occurrence == nil do
+    begin
+      filter = "resourceUrl=\"#{resource_url}\" AND noteProjectId=\"goog-analysis\" AND noteId=\"PACKAGE_VULNERABILITY\""
+      # [END containeranalysis_poll_discovery_occurrence_finished]i
+      # The above filter isn't testable, since it looks for occurrences in a locked down project
+      # Fall back to a more permissive filter for testing
+      filter = "kind = \"DISCOVERY\" AND resourceUrl = \"#{resource_url}\""
+      # [START containeranalysis_poll_discovery_occurrence_finished]
+      # Only the discovery occurrence should be returned for the given filter
+      discovery_occurrence = grafeas_v1_beta1_client.list_occurrences(formatted_parent, filter:filter).first
+    rescue StandardError # If there is an error, keep trying until the timeout deadline
+    ensure
+      # check for timeout
+      sleep 1
+      if Time.now > deadline
+        raise RuntimeError, 'Timeout while retrieving discovery occurrence.'
+      end
+    end
+  end
+
+  # Wait for the discovery occurrence to enter a terminal state
+  status = Grafeas::V1beta1::Discovery::Discovered::AnalysisStatus::PENDING
+  while (status != :FINISHED_SUCCESS && status != :FINISHED_FAILED && status != :FINISHED_UNSUPPORTED) do
+    # Update occurrence
+    begin
+      updated = grafeas_v1_beta1_client.get_occurrence(discovery_occurrence.name)
+      status = updated.discovered.discovered.analysis_status
+    rescue StandardError # If there is an error, keep trying until the timeout deadline
+    ensure
+      # check for timeout
+      sleep 1
+      if Time.now > deadline
+        raise RuntimeError, 'Timeout while retrieving discovery occurrence.'
+      end
+    end
+  end
+  puts "Found discovery occurrence #{updated.name}. Status: #{updated.discovered.discovered.analysis_status}"
+  # [END containeranalysis_poll_discovery_occurrence_finished]
+  return updated
+end
+
+def find_vulnerabilities_for_image resource_url:, project_id:
+  # [START containeranalysis_vulnerability_occurrences_for_image]
+  # resource_url = "The URL of the resource associated with the occurrence, eg. https://gcr.io/project/image@sha256:123"
+  # project_id   = "The Google Cloud project ID of the vulnerabilities to retrieve"
+
+  require "google/cloud/devtools/containeranalysis"
+  containerAnalysis = Google::Cloud::Devtools::Containeranalysis
+
+  # Initialize the client
+  grafeas_v1_beta1_client = containerAnalysis::GrafeasV1Beta1.new(version: :v1beta1)
+
+  formatted_parent = containerAnalysis::V1beta1::GrafeasV1Beta1Client.project_path(project_id)
+  filter = "resourceUrl = \"#{resource_url}\" AND kind = \"VULNERABILITY\""
+  occurrence_list = grafeas_v1_beta1_client.list_occurrences(formatted_parent, filter:filter)
+  # [END containeranalysis_vulnerability_occurrences_for_image]
+  return occurrence_list
+end
+
+def find_high_severity_vulnerabilities_for_image resource_url:, project_id:
+  # [START containeranalysis_filter_vulnerability_occurrences]
+  # resource_url = "The URL of the resource associated with the occurrence, eg. https://gcr.io/project/image@sha256:123"
+  # project_id   = "The Google Cloud project ID of the vulnerabilities to retrieve"
+
+  require "google/cloud/devtools/containeranalysis"
+  containerAnalysis = Google::Cloud::Devtools::Containeranalysis
+
+  # Initialize the client
+  grafeas_v1_beta1_client = containerAnalysis::GrafeasV1Beta1.new(version: :v1beta1)
+
+  formatted_parent = containerAnalysis::V1beta1::GrafeasV1Beta1Client.project_path(project_id)
+  filter = "resourceUrl = \"#{resource_url}\" AND kind = \"VULNERABILITY\""
+  vulnerability_list = grafeas_v1_beta1_client.list_occurrences(formatted_parent, filter:filter)
+  # Filter the list to include only "high" and "critical" vulnerabilities
+  high_severity_list = vulnerability_list.select{ |item| 
+    item.vulnerability.severity == :HIGH || item.vulnerability.severity == :CRITICAL  
+  }
+  # [END containeranalysis_filter_vulnerability_occurrences]
+  return high_severity_list
+end
---
+++ container-registry/container-analysis/spec/sample_spec.rb
@@ -0,0 +1,266 @@
+# Copyright 2019 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+$LOAD_PATH.unshift('./google-cloud-containeranalysis/lib')
+require "google/cloud/devtools/containeranalysis"
+require_relative "../sample"
+require "rspec"
+require "google/gax/grpc"
+require "pathname"
+require 'securerandom'
+
+ContainerAnalysis = Google::Cloud::Devtools::Containeranalysis
+
+describe "Container Analysis API samples" do
+  let(:client) { ContainerAnalysis::GrafeasV1Beta1.new(version: :v1beta1) }
+
+  before do |example|
+    test_name = example.description.tr(' ', '-')
+    uuid = SecureRandom.uuid
+    @note_id = "note-" + uuid + "-" + test_name
+    @image_url = "https://gcr.io/" + test_name + "/" + uuid
+    @project_id = "sanche-testing-project"
+    @note_obj = create_note project_id: @project_id, note_id: @note_id
+    @try_limit = 10
+    @sleep_time = 1
+    @subscription_id = "occurrence-subscription-" + uuid
+    @uuid = uuid
+  end
+
+  after do
+    begin
+      delete_note project_id: @project_id, note_id: @note_id
+    rescue
+    end
+  end
+
+  example "test create note" do
+    # note should be created as part of set up. verify that it succeeded
+    result_note = get_note project_id: @project_id, note_id: @note_id
+    expect(result_note.name).to eq(@note_obj.name)
+  end
+
+  example "test delete note" do
+    delete_note project_id: @project_id, note_id: @note_id
+    begin
+      get_note project_id: @project_id, note_id: @note_id
+      # get_note should throw exception. Test fails
+      assert(false)
+    rescue Google::Gax::RetryError
+      puts "rescued"
+      # test passes
+    end
+  end
+
+  example "test create occurrence" do
+    result = create_occurrence resource_url: @image_url, 
+                               note_id: @note_id,
+                               occurrence_project: @project_id,
+                               note_project: @project_id
+    expect(result).not_to be_nil
+    expect(result.resource.uri).to eq(@image_url)
+
+    occurrence_id = Pathname.new(result.name).basename.to_s
+    retrieved = get_occurrence occurrence_id: occurrence_id, project_id: @project_id
+    expect(retrieved).not_to be_nil
+    expect(retrieved.name).to eq(result.name)
+  end
+
+  example "test delete occurrence" do
+    created = create_occurrence resource_url: @image_url, 
+                                note_id: @note_id,
+                                occurrence_project: @project_id,
+                                note_project: @project_id
+    occurrence_id = Pathname.new(created.name).basename.to_s
+    delete_occurrence occurrence_id: occurrence_id, project_id: @project_id
+    begin
+      get_occurrence occurrence_id: occurrence_id, project_id: @project_id
+      # get_occurrence should throw exception on deleted occurrence. Test fails
+      assert(false)
+    rescue Google::Gax::RetryError
+      puts "rescued"
+      # test passes
+    end
+  end
+
+  example "test occurrences for image" do
+    count = get_occurrences_for_image resource_url: @image_url, project_id: @project_id
+    expect(count).to eq(0)
+
+    create_occurrence resource_url: @image_url, 
+                      note_id: @note_id,
+                      occurrence_project: @project_id,
+                      note_project: @project_id
+    try = 0
+    while count != 1 and try < @try_limit
+      sleep @sleep_time
+      count = get_occurrences_for_image resource_url: @image_url, project_id: @project_id
+      try += 1
+    end
+    expect(count).to eq(1)
+  end
+
+  example "test occurrences for note" do
+    count = get_occurrences_for_note note_id: @note_id, project_id: @project_id
+    expect(count).to eq(0)
+
+    create_occurrence resource_url: @image_url, 
+                      note_id: @note_id,
+                      occurrence_project: @project_id,
+                      note_project: @project_id
+    try = 0
+    while count != 1 and try < @try_limit
+      sleep @sleep_time
+      count = get_occurrences_for_note note_id: @note_id, project_id: @project_id
+      try += 1
+    end
+    expect(count).to eq(1)
+  end
+
+  example "test occurrence pubsub" do
+    try = 0
+    count = -1
+    # empty the pubsub queue
+    while count != 0 and try < @try_limit
+      count = occurrence_pubsub subscription_id: @subscription_id, timeout_seconds: 5, project_id: @project_id
+      try += 1
+    end
+    expect(count).to eq(0)
+
+    # test pubsub while creating occurrences
+    try = 0
+    total_num = 3
+    while count != total_num and try < @try_limit
+      # start the pubsub function listening in its own thread
+      t2 = Thread.new{
+        Thread.current[:output] = occurrence_pubsub subscription_id: @subscription_id, 
+                                                    timeout_seconds: (total_num*@sleep_time)+10,
+                                                    project_id: @project_id
+      }
+      sleep 5
+      # create a number of test occurrences
+      (1..total_num).each do |counter|
+          created = create_occurrence resource_url: @image_url, 
+                                      note_id: @note_id,
+                                      occurrence_project: @project_id,
+                                      note_project: @project_id
+          sleep @sleep_time
+          occurrence_id = Pathname.new(created.name).basename.to_s
+          delete_occurrence occurrence_id: occurrence_id, project_id: @project_id
+      end
+      # check to ensure the numbers match
+      t2.join
+      count = t2[:output]
+    end
+    expect(count).to eq(total_num)
+  end
+
+  example "test polling discovery occurrence" do
+    begin
+      poll_discovery_finished resource_url: @image_url,
+                              project_id: @project_id,
+                              timeout_seconds: 5
+      # expect poll to fil when resource has no discovery occurrence
+      assert(false)
+    rescue RuntimeError
+      puts "rescued"
+      # test passes
+    end
+
+    # create discovery occurrence
+    note_id = "discovery-note-" + @uuid
+    grafeas_v1_beta1_client = ContainerAnalysis::GrafeasV1Beta1.new(version: :v1beta1)
+    formatted_project = ContainerAnalysis::V1beta1::GrafeasV1Beta1Client.project_path(@project_id)
+    note = {discovery: {}}
+    grafeas_v1_beta1_client.create_note(formatted_project, note_id, note)
+    formatted_note = ContainerAnalysis::V1beta1::GrafeasV1Beta1Client.note_path(@project_id, note_id)
+    occurrence = {note_name: formatted_note, 
+                  discovered: {
+                    discovered: {analysis_status: :FINISHED_SUCCESS}, 
+                  },
+                  resource:{uri: @image_url}}
+    created = grafeas_v1_beta1_client.create_occurrence(formatted_project, occurrence)
+
+
+    # poll again
+    found = poll_discovery_finished resource_url: @image_url,
+                                    project_id: @project_id,
+                                    timeout_seconds: 5
+    expect(found.name).to eq(created.name)
+    expect(found.discovered.discovered.analysis_status).to eq(:FINISHED_SUCCESS)
+
+    # clean up
+    occurrence_id = Pathname.new(created.name).basename.to_s
+    delete_occurrence occurrence_id: occurrence_id, project_id: @project_id
+    delete_note project_id: @project_id, note_id: note_id
+  end
+
+  example "test find vulnerabilities" do
+    result_list = find_vulnerabilities_for_image resource_url: @image_url, project_id: @project_id
+    c = result_list.count
+    expect(c).to eq(0)
+
+    # create vulnerability occurrence
+    create_occurrence resource_url: @image_url, 
+                      note_id: @note_id,
+                      occurrence_project: @project_id,
+                      note_project: @project_id
+    try = 0
+    while c != 1 and try < @try_limit
+      sleep @sleep_time
+      result_list = find_vulnerabilities_for_image resource_url: @image_url, project_id: @project_id
+      c = result_list.count
+      try += 1
+    end
+    expect(result_list.count).to eq(1)
+  end
+
+  example "test find high severity vulnerabilities" do
+    result_list = find_high_severity_vulnerabilities_for_image resource_url: @image_url, project_id: @project_id
+    sleep 1
+    expect(result_list.count).to eq(0)
+ 
+    # create vulnerability occurrence
+    create_occurrence resource_url: @image_url, 
+                      note_id: @note_id,
+                      occurrence_project: @project_id,
+                      note_project: @project_id
+    result_list = find_high_severity_vulnerabilities_for_image resource_url: @image_url, project_id: @project_id
+    sleep 1
+    expect(result_list.count).to eq(0)
+
+    # create critical secerity occurrence
+    note_id = "severe-note-" + @uuid
+    grafeas_v1_beta1_client = ContainerAnalysis::GrafeasV1Beta1.new(version: :v1beta1)
+    formatted_project = ContainerAnalysis::V1beta1::GrafeasV1Beta1Client.project_path(@project_id)
+    note = {vulnerability: {severity: :CRITICAL}}
+    grafeas_v1_beta1_client.create_note(formatted_project, note_id, note)
+    formatted_note = ContainerAnalysis::V1beta1::GrafeasV1Beta1Client.note_path(@project_id, note_id)
+    occurrence = {note_name: formatted_note, 
+                  vulnerability: {severity: :CRITICAL},
+                  resource:{uri: @image_url}}
+    grafeas_v1_beta1_client.create_occurrence(formatted_project, occurrence)
+
+    # try again
+    try = 0
+    c = 0
+    while c != 1 and try < @try_limit
+      sleep @sleep_time
+      result_list = find_high_severity_vulnerabilities_for_image resource_url: @image_url, project_id: @project_id
+      c = result_list.count
+      try += 1
+    end
+    expect(result_list.count).to eq(1)
+  end
+end
