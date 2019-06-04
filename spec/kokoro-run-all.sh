5c9e5f9657ec82c29500c57f1915ca08ee0a0b60
Tyler Bui-Palsulich <tbp@google.com>
2019-06-04 13:44:57 -0400

    spec: fix env var checking and secondary project

--- spec/kokoro-run-all.sh
+++ spec/kokoro-run-all.sh
@@ -19,14 +19,8 @@ source $KOKORO_GFILE_DIR/secrets.sh
 export BUNDLE_GEMFILE=
 
 for REQUIRED_VARIABLE in
-  GOOGLE_CLOUD_PROJECT
-  GOOGLE_APPLICATION_CREDENTIALS
-  GOOGLE_CLOUD_STORAGE_BUCKET
-  ALTERNATE_GOOGLE_CLOUD_STORAGE_BUCKET
-  GOOGLE_CLOUD_PROJECT_SECONDARY
-  GOOGLE_APPLICATION_CREDENTIALS_SECONDARY
-  GOOGLE_CLOUD_KMS_KEY_NAME
-  GOOGLE_CLOUD_KMS_KEY_RING
+  GOOGLE_CLOUD_KMS_KEY_NAME \
+  GOOGLE_CLOUD_KMS_KEY_RING \
 do
   if [[ -z "${REQUIRED_VARIABLE:-}" ]]; then
     echo "Must set $REQUIRED_VARIABLE"
@@ -55,15 +49,12 @@ fi
 echo "Running tests in project $GOOGLE_CLOUD_PROJECT";
 trap "gimmeproj -project cloud-samples-ruby-test-kokoro done $GOOGLE_CLOUD_PROJECT" EXIT
 
+export GOOGLE_CLOUD_PROJECT_SECONDARY=cloud-samples-ruby-test-second
+export GOOGLE_APPLICATION_CREDENTIALS_SECONDARY=$KOKORO_GFILE_DIR/cloud-samples-ruby-test-second-ede32e88c59c.json
+
 # Set application credentials to the project-specific account. Some APIs do not
 # allow the service account project and GOOGLE_CLOUD_PROJECT to be different.
 export GOOGLE_APPLICATION_CREDENTIALS=$KOKORO_KEYSTORE_DIR/71386_kokoro-$GOOGLE_CLOUD_PROJECT
-export GOOGLE_APPLICATION_CREDENTIALS_SECONDARY=$KOKORO_KEYSTORE_DIR/71386_kokoro-cloud-samples-ruby-test-0
-
-# Make sure the secondary service account is different than the normal account.
-if [[ $GOOGLE_CLOUD_PROJECT = "cloud-samples-ruby-test-0" ]]; then
-  export GOOGLE_APPLICATION_CREDENTIALS_SECONDARY=$KOKORO_KEYSTORE_DIR/71386_kokoro-cloud-samples-ruby-test-1
-fi
 
 export FIRESTORE_PROJECT_ID=ruby-firestore
 export E2E_GOOGLE_CLOUD_PROJECT=cloud-samples-ruby-test-kokoro
