71d034bb4b78f738a3707716999d4e5c24be4da8
Billy Jacobson <thebilly@google.com>
2020-01-27 17:34:02 -0500

    Read snippets

---
+++ bigtable/read_samples.rb
@@ -0,0 +1,112 @@
+# frozen_string_literal: true
+
+# Copyright 2019 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+
+# Import google bigtable client lib
+require "google/cloud/bigtable"
+
+# [START bigtable_reads_row]
+# [START bigtable_reads_row_partial]
+# [START bigtable_reads_rows]
+# [START bigtable_reads_row_range]
+# [START bigtable_reads_row_ranges]
+# [START bigtable_reads_prefix]
+# [START bigtable_reads_filter]
+
+
+def reads_row project_id, instance_id, table_id
+  bigtable = Google::Cloud::Bigtable.new project_id: project_id
+  table = bigtable.table instance_id, table_id
+
+  row = table.read_row "phone#4c410523#20190501"
+  print_row row
+end
+
+def reads_row_partial project_id, instance_id, table_id
+  bigtable = Google::Cloud::Bigtable.new project_id: project_id
+  table = bigtable.table instance_id, table_id
+  filter = Google::Cloud::Bigtable::RowFilter.qualifier("os_build")
+
+  row = table.read_row "phone#4c410523#20190501", filter: filter
+  print_row row
+end
+
+def reads_rows project_id, instance_id, table_id
+  bigtable = Google::Cloud::Bigtable.new project_id: project_id
+  table = bigtable.table instance_id, table_id
+
+  table.read_rows(keys: ["phone#4c410523#20190501", "phone#4c410523#20190502"]).each do |row|
+    print_row row
+  end
+
+end
+
+def reads_row_range project_id, instance_id, table_id
+  bigtable = Google::Cloud::Bigtable.new project_id: project_id
+  table = bigtable.table instance_id, table_id
+
+  range = table.new_row_range.between("phone#4c410523#20190501", "phone#4c410523#201906201")
+  table.read_rows(ranges: range).each do |row|
+    print_row row
+  end
+end
+
+def reads_row_ranges project_id, instance_id, table_id
+  bigtable = Google::Cloud::Bigtable.new project_id: project_id
+  table = bigtable.table instance_id, table_id
+
+  ranges = []
+  ranges <<
+      table.new_row_range.between("phone#4c410523#20190501", "phone#4c410523#201906201")
+  table.new_row_range.between("phone#5c10102#20190501", "phone#5c10102#201906201")
+  table.read_rows(ranges: ranges).each do |row|
+    print_row row
+  end
+end
+
+def reads_prefix project_id, instance_id, table_id
+  bigtable = Google::Cloud::Bigtable.new project_id: project_id
+  table = bigtable.table instance_id, table_id
+
+  prefix = "phone#"
+  range = table.new_row_range.between(prefix, prefix.next)
+  table.read_rows(ranges: range).each do |row|
+    print_row row
+  end
+end
+
+def reads_filter project_id, instance_id, table_id
+  bigtable = Google::Cloud::Bigtable.new project_id: project_id
+  table = bigtable.table instance_id, table_id
+
+  filter = Google::Cloud::Bigtable::RowFilter.value("PQ2A.*$")
+  table.read_rows(filter: filter).each do |row|
+    print_row row
+  end
+end
+
+def print_row row
+  puts "Reading data for #{row.key}"
+
+  row.cells.each do |column_family, data|
+    puts "Column Family #{column_family}"
+    data.each do |cell|
+      labels = cell.labels.length ? ` [#{cell.labels.join(',')}]` : ''
+      puts "\t#{cell.qualifier}: #{cell.value} #{cell.timestamp}#{labels}"
+    end
+    puts "\n"
+  end
+end
---
+++ bigtable/spec/read_samples_spec.rb
@@ -0,0 +1,212 @@
+require_relative "spec_helper"
+require "securerandom"
+require "google/cloud/bigtable"
+require "google/cloud/bigtable/admin"
+require_relative "../read_samples"
+
+describe "Google Cloud Bigtable Read Samples", focus: true do
+  before(:all) do
+    @table_id = "mobile-time-series-#{SecureRandom.hex 8}"
+    bigtable = Google::Cloud::Bigtable.new project_id: @project_id
+
+    puts "Creating table."
+    column_families = Google::Cloud::Bigtable::ColumnFamilyMap.new
+    column_families.add "stats_summary", gc_rule: nil
+
+    @table = bigtable.create_table @instance_id, @table_id, column_families: column_families
+
+    @timestamp = (Time.now.to_f * 1_000_000).round(-3)
+
+    entries = []
+    entries <<
+        @table.new_mutation_entry("phone#4c410523#20190501").set_cell("stats_summary", "connected_cell", 1, timestamp: @timestamp)
+            .set_cell("stats_summary", "connected_wifi", 1, timestamp: @timestamp)
+            .set_cell("stats_summary", "os_build", "PQ2A.190405.003", timestamp: @timestamp) <<
+        @table.new_mutation_entry("phone#4c410523#20190502").set_cell("stats_summary", "connected_cell", 1, timestamp: @timestamp)
+            .set_cell("stats_summary", "connected_wifi", 1, timestamp: @timestamp)
+            .set_cell("stats_summary", "os_build", "PQ2A.190405.004", timestamp: @timestamp) <<
+        @table.new_mutation_entry("phone#4c410523#20190505").set_cell("stats_summary", "connected_cell", 0, timestamp: @timestamp)
+            .set_cell("stats_summary", "connected_wifi", 1, timestamp: @timestamp)
+            .set_cell("stats_summary", "os_build", "PQ2A.190406.000", timestamp: @timestamp) <<
+        @table.new_mutation_entry("phone#5c10102#20190501").set_cell("stats_summary", "connected_cell", 1, timestamp: @timestamp)
+            .set_cell("stats_summary", "connected_wifi", 1, timestamp: @timestamp)
+            .set_cell("stats_summary", "os_build", "PQ2A.190401.002", timestamp: @timestamp) <<
+        @table.new_mutation_entry("phone#5c10102#20190502").set_cell("stats_summary", "connected_cell", 1, timestamp: @timestamp)
+            .set_cell("stats_summary", "connected_wifi", 0, timestamp: @timestamp)
+            .set_cell("stats_summary", "os_build", "PQ2A.190406.000", timestamp: @timestamp)
+
+    @table.mutate_rows entries
+  end
+
+  it "reads_row" do
+    output = capture do
+      reads_row @project_id, @instance_id, @table_id
+    end
+    expect(output).to match `Reading data for phone#4c410523#20190501:
+Column Family stats_summary
+	connected_cell: 1 @#{@timestamp}
+	connected_wifi: 1 @#{@timestamp}
+	os_build: PQ2A.190405.003 @#{@timestamp}`
+  end
+
+  it "reads_row_partial" do
+    output = capture do
+      reads_row_partial @project_id, @instance_id, @table_id
+    end
+    expect(output).to match `
+Reading data for phone#4c410523#20190501:
+Column Family stats_summary
+\tconnected_cell: 1 @#{@timestamp}
+                            \tconnected_wifi: 1 @#{@timestamp}
+                            \tos_build: PQ2A.190405.003 @#{@timestamp}
+
+
+                            `
+  end
+
+  it "reads_rows" do
+    output = capture do
+      reads_rows @project_id, @instance_id, @table_id
+    end
+    expect(output).to match `
+Reading data for phone#4c410523#20190501:
+Column Family stats_summary
+\tos_build: PQ2A.190405.003 @#{@timestamp}
+
+
+                            `
+  end
+
+  it "reads_row_range" do
+    output = capture do
+      reads_row_range @project_id, @instance_id, @table_id
+    end
+    expect(output).to match `
+Reading data for phone#4c410523#20190501:
+Column Family stats_summary
+\tconnected_cell: 1 @#{@timestamp}
+                            \tconnected_wifi: 1 @#{@timestamp}
+                            \tos_build: PQ2A.190405.003 @#{@timestamp}
+
+Reading data for phone#4c410523#20190502:
+Column Family stats_summary
+\tconnected_cell: 1 @#{@timestamp}
+                            \tconnected_wifi: 1 @#{@timestamp}
+                            \tos_build: PQ2A.190405.004 @#{@timestamp}
+
+
+                            `
+  end
+
+  it "reads_row_ranges" do
+    output = capture do
+      reads_row_ranges @project_id, @instance_id, @table_id
+    end
+    expect(output).to match `
+Reading data for phone#4c410523#20190501:
+Column Family stats_summary
+\tconnected_cell: 1 @#{@timestamp}
+                            \tconnected_wifi: 1 @#{@timestamp}
+                            \tos_build: PQ2A.190405.003 @#{@timestamp}
+
+Reading data for phone#4c410523#20190502:
+Column Family stats_summary
+\tconnected_cell: 1 @#{@timestamp}
+                            \tconnected_wifi: 1 @#{@timestamp}
+                            \tos_build: PQ2A.190405.004 @#{@timestamp}
+
+Reading data for phone#4c410523#20190505:
+Column Family stats_summary
+\tconnected_cell: 0 @#{@timestamp}
+                            \tconnected_wifi: 1 @#{@timestamp}
+                            \tos_build: PQ2A.190406.000 @#{@timestamp}
+
+
+                            `
+  end
+
+  it "reads_prefix" do
+    output = capture do
+      reads_prefix @project_id, @instance_id, @table_id
+    end
+    expect(output).to match `
+Reading data for phone#4c410523#20190501:
+Column Family stats_summary
+\tconnected_cell: 1 @#{@timestamp}
+                            \tconnected_wifi: 1 @#{@timestamp}
+                            \tos_build: PQ2A.190405.003 @#{@timestamp}
+
+Reading data for phone#4c410523#20190502:
+Column Family stats_summary
+\tconnected_cell: 1 @#{@timestamp}
+                            \tconnected_wifi: 1 @#{@timestamp}
+                            \tos_build: PQ2A.190405.004 @#{@timestamp}
+
+Reading data for phone#4c410523#20190505:
+Column Family stats_summary
+\tconnected_cell: 0 @#{@timestamp}
+                            \tconnected_wifi: 1 @#{@timestamp}
+                            \tos_build: PQ2A.190406.000 @#{@timestamp}
+
+Reading data for phone#5c10102#20190501:
+Column Family stats_summary
+\tconnected_cell: 1 @#{@timestamp}
+                            \tconnected_wifi: 1 @#{@timestamp}
+                            \tos_build: PQ2A.190401.002 @#{@timestamp}
+
+Reading data for phone#5c10102#20190502:
+Column Family stats_summary
+\tconnected_cell: 1 @#{@timestamp}
+                            \tconnected_wifi: 0 @#{@timestamp}
+                            \tos_build: PQ2A.190406.000 @#{@timestamp}
+
+
+                            `
+  end
+
+  it "reads_filter" do
+    output = capture do
+      reads_filter @project_id, @instance_id, @table_id
+    end
+    expect(output).to match `
+Reading data for phone#4c410523#20190501:
+Column Family stats_summary
+\tconnected_cell: 1 @#{@timestamp}
+                            \tconnected_wifi: 1 @#{@timestamp}
+                            \tos_build: PQ2A.190405.003 @#{@timestamp}
+
+Reading data for phone#4c410523#20190502:
+Column Family stats_summary
+\tconnected_cell: 1 @#{@timestamp}
+                            \tconnected_wifi: 1 @#{@timestamp}
+                            \tos_build: PQ2A.190405.004 @#{@timestamp}
+
+Reading data for phone#4c410523#20190505:
+Column Family stats_summary
+\tconnected_cell: 0 @#{@timestamp}
+                            \tconnected_wifi: 1 @#{@timestamp}
+                            \tos_build: PQ2A.190406.000 @#{@timestamp}
+
+Reading data for phone#5c10102#20190501:
+Column Family stats_summary
+\tconnected_cell: 1 @#{@timestamp}
+                            \tconnected_wifi: 1 @#{@timestamp}
+                            \tos_build: PQ2A.190401.002 @#{@timestamp}
+
+Reading data for phone#5c10102#20190502:
+Column Family stats_summary
+\tconnected_cell: 1 @#{@timestamp}
+                            \tconnected_wifi: 0 @#{@timestamp}
+                            \tos_build: PQ2A.190406.000 @#{@timestamp}
+
+
+                            `
+  end
+
+
+  after(:all) do
+    puts "Deleting table."
+    @table.delete
+  end
+
+end
