78cf1af2fb212fc3cdaefc7d0e53b787324b9a1d
Jiren Patel <jirenpatel@gmail.com>
2020-01-28 18:26:04 +0530

    fix: query with array of struct due to list order issue

--- spanner/spec/spanner_samples_spec.rb
+++ spanner/spec/spanner_samples_spec.rb
@@ -234,7 +234,9 @@ describe "Google Cloud Spanner API samples" do
                                  instance_id: @instance.instance_id,
                                  database_id: database.database_id
     end
-    expect(captured_output).to match /8\n7\n6\n/
+    expect(captured_output).to include "6"
+    expect(captured_output).to include "7"
+    expect(captured_output).to include "8"
   end
 
   example "query struct field" do
