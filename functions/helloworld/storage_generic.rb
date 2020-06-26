c050e62fe291813b711ac189e849679c334b67c8
Chris Smith <quartzmo@gmail.com>
2020-06-26 16:25:41 -0600

    feat(functions): Add functions_helloworld_storage_generic

---
+++ functions/helloworld/storage_generic.rb
@@ -0,0 +1,31 @@
+# Copyright 2020 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# [START functions_helloworld_storage_generic]
+require "functions_framework"
+
+FunctionsFramework.cloud_event "hello-gcs-generic" do |event|
+  # The event parameter is a FunctionsFramework::CloudEvents::Event object.
+  # See https://www.rubydoc.info/gems/functions_framework/FunctionsFramework/CloudEvents/Event
+  payload = event.data
+
+  FunctionsFramework.logger.info "Event: #{event.id}"
+  FunctionsFramework.logger.info "Event Type: #{event.type}"
+  FunctionsFramework.logger.info "Bucket: #{payload['bucket']}"
+  FunctionsFramework.logger.info "File: #{payload['name']}"
+  FunctionsFramework.logger.info "Metageneration: #{payload['metageneration']}"
+  FunctionsFramework.logger.info "Created: #{payload['timeCreated']}"
+  FunctionsFramework.logger.info "Updated: #{payload['updated']}"
+end
+# [END functions_helloworld_storage_generic]
---
+++ functions/test/helloworld/storage_generic_test.rb
@@ -0,0 +1,48 @@
+# Copyright 2020 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+require "helper"
+require "date"
+
+describe "functions_helloworld_storage" do
+  include FunctionsFramework::Testing
+
+  let(:source) { "//storage.googleapis.com/projects/sample-project/buckets/sample-bucket/objects/ruby-rocks.rb" }
+  let(:type) { "google.storage.object.change.v1" }
+
+  it "responds to generic event" do
+    load_temporary "helloworld/storage_generic.rb" do
+      timestamp = DateTime.new(2020, 2, 3, 4, 5, 6).rfc3339
+      payload = {
+        "bucket"         => "sample-bucket",
+        "name"           => "ruby-rocks.rb",
+        "metageneration" => "1",
+        "timeCreated"    => timestamp,
+        "updated"        => timestamp
+      }
+      event = make_cloud_event payload, source: source, type: type
+      _out, err = capture_subprocess_io do
+        call_event "hello-gcs-generic", event
+      end
+
+      assert_match(/Event: /, err)
+      assert_match(/Event Type: google.storage.object.change.v1/, err)
+      assert_match(/Bucket: sample-bucket/, err)
+      assert_match(/File: ruby-rocks.rb/, err)
+      assert_match(/Metageneration: 1/, err)
+      assert_match(/Created: 2020-02-03T04:05:06\+00:00/, err)
+      assert_match(/Updated: 2020-02-03T04:05:06\+00:00/, err)
+    end
+  end
+end
