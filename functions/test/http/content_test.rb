3757c35caf2d2889d07aba5f6767cb6042f0c4ce
Viacheslav Rostovtsev <virost@google.com>
2020-06-26 20:32:35 +0000

    http_content_methods

---
+++ functions/test/http/content_test.rb
@@ -0,0 +1,55 @@
+# Copyright 2020 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+require "helper"
+
+describe "functions_http_content" do
+  include FunctionsFramework::Testing
+
+  it "handles name in the JSON body" do
+    load_temporary "http/content.rb" do
+      request = make_post_request "http://example.com:8080/", '{"name": "Ruby"}', ["content-type:application/json"]
+      response = call_http "http-content", request
+      assert_equal 200, response.status
+      assert_equal "Hello Ruby!", response.body.join
+    end
+  end
+
+  it "handles name in the octet-stream" do
+    load_temporary "http/content.rb" do
+      request = make_post_request "http://example.com:8080/", 'Ruby', ["content-type:application/octet-stream"]
+      response = call_http "http-content", request
+      assert_equal 200, response.status
+      assert_equal "Hello Ruby!", response.body.join
+    end
+  end
+
+  it "handles name in the plaintext body" do
+    load_temporary "http/content.rb" do
+      request = make_post_request "http://example.com:8080/", 'Ruby', ["content-type:text/plain"]
+      response = call_http "http-content", request
+      assert_equal 200, response.status
+      assert_equal "Hello Ruby!", response.body.join
+    end
+  end
+
+  it "handles name in the form-urlencoded body" do
+    load_temporary "http/content.rb" do
+      request = make_post_request "http://example.com:8080/", 'name=Ruby', ["content-type:application/x-www-form-urlencoded"]
+      response = call_http "http-content", request
+      assert_equal 200, response.status
+      assert_equal "Hello Ruby!", response.body.join
+    end
+  end
+end
---
+++ functions/test/http/method_test.rb
@@ -0,0 +1,49 @@
+# Copyright 2020 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+require "helper"
+require "json"
+
+describe "functions_http_method" do
+  include FunctionsFramework::Testing
+
+  it "handles GET requests" do
+    load_temporary "http/method.rb" do
+      request = make_get_request "http://example.com:8080/"
+      response = call_http "http-method", request
+      assert_equal 200, response.status
+      assert_equal "Hello World!", response.body.join
+    end
+  end
+
+  it "handles PUT requests" do
+    load_temporary "http/method.rb" do
+      request = make_post_request "http://example.com:8080/", "Ruby", ["content-type:text/plain"]
+      request.env[::Rack::REQUEST_METHOD] = ::Rack::PUT
+
+      response = call_http "http-method", request
+      assert_equal 403, response.status
+      assert_equal "Forbidden!", response.body.join
+    end
+  end
+
+  it "handles POST requests" do
+    load_temporary "http/method.rb" do
+      request = make_post_request "http://example.com:8080/", "Ruby", ["content-type:text/plain"]
+      response = call_http "http-method", request
+      assert_equal 405, response.status
+      assert_equal "Something blew up!", JSON.parse(response.body.read.to_s)["error"]
+    end
+  end
+end
