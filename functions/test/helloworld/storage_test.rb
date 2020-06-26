baefd999bc1efe0cc270b6fea7158e1f0d12da6c
Chris Smith <quartzmo@gmail.com>
2020-06-26 16:28:01 -0600

    feat(functions): Add unit test samples (#620)
    * Add storage_unit_test sample
    * Add pubsub_unit_test sample
    * Add http_unit_test sample

--- functions/test/helloworld/storage_test.rb
+++ functions/test/helloworld/storage_test.rb
@@ -12,14 +12,52 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-require "helper"
-require "base64"
+# [START functions_storage_unit_test]
+require "minitest/autorun"
+require "functions_framework/testing"
 
 describe "functions_helloworld_storage" do
   include FunctionsFramework::Testing
 
   let(:source) { "//storage.googleapis.com/projects/sample-project/buckets/sample-bucket/objects/ruby-rocks.rb" }
   let(:type) { "google.storage.object.change.v1" }
+
+  it "responds to uploaded event" do
+    load_temporary "helloworld/storage.rb" do
+      payload = {
+        "name"           => "ruby-rocks.rb",
+        "metageneration" => "1"
+      }
+      event = make_cloud_event payload, source: source, type: type
+      _out, err = capture_subprocess_io do
+        # Call tested function
+        call_event "hello-gcs", event
+      end
+      assert_match(/File ruby-rocks\.rb uploaded\./, err)
+    end
+  end
+end
+# [END functions_storage_unit_test]
+
+describe "functions_helloworld_storage" do
+  include FunctionsFramework::Testing
+
+  let(:source) { "//storage.googleapis.com/projects/sample-project/buckets/sample-bucket/objects/ruby-rocks.rb" }
+  let(:type) { "google.storage.object.change.v1" }
+
+  it "responds to updated event" do
+    load_temporary "helloworld/storage.rb" do
+      payload = {
+        "name"           => "ruby-rocks.rb",
+        "metageneration" => "2"
+      }
+      event = make_cloud_event payload, source: source, type: type
+      _out, err = capture_subprocess_io do
+        call_event "hello-gcs", event
+      end
+      assert_match(/File ruby-rocks\.rb metadata updated\./, err)
+    end
+  end
 
   it "responds to deleted event" do
     load_temporary "helloworld/storage.rb" do
@@ -33,34 +71,6 @@ describe "functions_helloworld_storage" do
         call_event "hello-gcs", event
       end
       assert_match(/File ruby-rocks\.rb deleted\./, err)
-    end
-  end
-
-  it "responds to uploaded event" do
-    load_temporary "helloworld/storage.rb" do
-      payload = {
-        "name"           => "ruby-rocks.rb",
-        "metageneration" => "1"
-      }
-      event = make_cloud_event payload, source: source, type: type
-      _out, err = capture_subprocess_io do
-        call_event "hello-gcs", event
-      end
-      assert_match(/File ruby-rocks\.rb uploaded\./, err)
-    end
-  end
-
-  it "responds to updated event" do
-    load_temporary "helloworld/storage.rb" do
-      payload = {
-        "name"           => "ruby-rocks.rb",
-        "metageneration" => "2"
-      }
-      event = make_cloud_event payload, source: source, type: type
-      _out, err = capture_subprocess_io do
-        call_event "hello-gcs", event
-      end
-      assert_match(/File ruby-rocks\.rb metadata updated\./, err)
     end
   end
 end
