ba75a300257b4ea84ab23905201fa25adb9d2065
averikitsch <akitsch@google.com>
2020-08-10 11:04:32 -0700

    lint

--- run/helloworld/spec/e2e_spec.rb
+++ run/helloworld/spec/e2e_spec.rb
@@ -12,22 +12,22 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-require 'rspec'
-require 'securerandom'
-require 'rest-client'
+require "rspec"
+require "securerandom"
+require "rest-client"
 
-describe 'E2E tests' do
+describe "E2E tests" do
   before :all do
     suffix = SecureRandom.hex(15)
-    system("gcloud", "builds", "submit", "--project=#{ENV['GOOGLE_CLOUD_PROJECT']}", "--config=e2e_test_setup.yaml", "--substitutions=_SUFFIX=#{suffix}", "--quiet")
+    system("gcloud", "builds", "submit", "--project=#{ENV["GOOGLE_CLOUD_PROJECT"]}", "--config=e2e_test_setup.yaml", "--substitutions=_SUFFIX=#{suffix}", "--quiet")
     @service = "helloworld-#{suffix}"
-    io = IO.popen(["gcloud", "run", "services", "describe", "--project=#{ENV['GOOGLE_CLOUD_PROJECT']}", @service, "--format=value(status.url)"])
+    io = IO.popen(["gcloud", "run", "services", "describe", "--project=#{ENV["GOOGLE_CLOUD_PROJECT"]}", @service, "--format=value(status.url)"])
     url = io.read
     @url = url[0..-2] # Strip newline character
     io.close
 
     if !@url
-      throw Error 'No service url found. For example: https://service-x8xabcdefg-uc.a.run.app'
+      throw Error "No service url found. For example: https://service-x8xabcdefg-uc.a.run.app"
     end
 
     io = IO.popen(["gcloud", "auth", "print-identity-token"])
@@ -36,12 +36,12 @@ describe 'E2E tests' do
   end
 
   after (:all) do
-    system("gcloud", "run", "services", "delete", @service, "--project=#{ENV['GOOGLE_CLOUD_PROJECT']}", "--platform=managed", "--region=us-central1", "--quiet")
+    system("gcloud", "run", "services", "delete", @service, "--project=#{ENV["GOOGLE_CLOUD_PROJECT"]}", "--platform=managed", "--region=us-central1", "--quiet")
   end
 
-  it 'Can make request to service' do
+  it "Can make request to service" do
     response = RestClient.get @url, Authorization: "Bearer #{@token}"
-    expect(response.body).to eq('Hello Test!')
+    expect(response.body).to eq("Hello Test!")
     expect(response.code).to eq(200)
   end
 
