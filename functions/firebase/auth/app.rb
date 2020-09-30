9d641f7e6d76e42985faba7ba0410cbcfe1949cf
Daniel Azuma <dazuma@google.com>
2020-09-30 00:16:54 +0000

    Update event param comments

--- functions/firebase/auth/app.rb
+++ functions/firebase/auth/app.rb
@@ -17,8 +17,9 @@ require "functions_framework"
 
 # Triggered by creation or deletion of a Firebase Auth user object.
 FunctionsFramework.cloud_event "hello_auth" do |event|
-  # The event parameter is a CloudEvents::Event::V1 object.
+  # Event-triggered Ruby functions receive a CloudEvents::Event::V1 object.
   # See https://cloudevents.github.io/sdk-ruby/latest/CloudEvents/Event/V1.html
+  # The Firebase event payload can be obtained from the `data` field.
   payload = event.data
 
   logger.info "Function triggered by creation/deletion of user: #{payload['uid']}"
