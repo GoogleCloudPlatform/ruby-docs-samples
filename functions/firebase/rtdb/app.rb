2aabfbacb29a0a48f12bb27a8f042aba1cc80e88
Daniel Azuma <dazuma@google.com>
2020-10-07 11:14:18 -0700

    feat: Add functions_firebase_rtdb sample

---
+++ functions/firebase/rtdb/app.rb
@@ -0,0 +1,36 @@
+# Copyright 2020 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# [START functions_firebase_rtdb]
+require "functions_framework"
+
+# Triggered by a change to a Firebase RTDB document.
+FunctionsFramework.cloud_event "hello_rtdb" do |event|
+  # Event-triggered Ruby functions receive a CloudEvents::Event::V1 object.
+  # See https://cloudevents.github.io/sdk-ruby/latest/CloudEvents/Event/V1.html
+  # The Firebase event payload can be obtained from the `data` field.
+  payload = event.data
+
+  if payload.key? "params"
+    logger.info "Path parameters:"
+    payload["params"].each do |param, value|
+      logger.info "  #{param}: #{value}"
+    end
+  end
+
+  logger.info "Function triggered by change to: #{event.source}"
+  logger.info "Admin?: #{payload.fetch 'admin', false}"
+  logger.info "Delta: #{payload['delta']}"
+end
+# [END functions_firebase_rtdb]
---
+++ functions/test/firebase/rtdb_test.rb
@@ -0,0 +1,40 @@
+# Copyright 2020 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+require "helper"
+
+describe "functions_firebase_rtdb" do
+  include FunctionsFramework::Testing
+
+  let(:source) { "//firebase.googleapis.com/projects/_/instances/my-project-id/refs/gcf-test/xyz" }
+  let(:type) { "google.firebase.database.document.v1.updated" }
+
+  it "responds to firestore rtdb update event" do
+    load_temporary "firebase/rtdb/app.rb" do
+      payload = {
+        "params" => { "child" => "abcde" },
+        "delta"  => { "foo" => "bar" }
+      }
+      event = make_cloud_event payload, source: source, type: type
+      _out, err = capture_subprocess_io do
+        call_event "hello_rtdb", event
+      end
+
+      assert_includes err, "Function triggered by change to: #{source}"
+      assert_match(/Path parameters:\n[^\n]+child: abcde\n/, err)
+      assert_includes err, "Admin?: false"
+      assert_includes err, 'Delta: {"foo"=>"bar"}'
+    end
+  end
+end
