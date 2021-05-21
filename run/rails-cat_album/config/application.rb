0a2a4c097a75f93e6ae231d198a07503c067c980
Tram Bui <trambui@google.com>
2021-05-21 13:37:04 -0500

    chore: check to see if master.key is present in dir for local dev

--- run/rails-cat_album/config/application.rb
+++ run/rails-cat_album/config/application.rb
@@ -19,6 +19,11 @@ require "rails/test_unit/railtie"
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
+key_file = File.join("config", "master.key")
+if File.exist?(key_file)
+  ENV["RAILS_MASTER_KEY"] = File.read(key_file)
+end
+
 module RailsCatAlbum
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
