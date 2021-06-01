21b405e33b1406c6c0abddcd87abc35bffb97371
Tram Bui <trambui@google.com>
2021-06-01 11:46:47 -0500

    remove the secret manager gem from Gemfile and Gemfile.lock since we don't need it anymore and API call code in application.rb

--- run/rails-cat_album/config/application.rb
+++ run/rails-cat_album/config/application.rb
@@ -7,14 +7,10 @@ require "active_job/railtie"
 require "active_record/railtie"
 require "active_storage/engine"
 require "action_controller/railtie"
-# require "action_mailer/railtie"
-# require "action_mailbox/engine"
-# require "action_text/engine"
 require "action_view/railtie"
-# require "action_cable/engine"
 require "sprockets/railtie"
 require "rails/test_unit/railtie"
-# require "google/cloud/secret_manager"
+
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
@@ -24,29 +20,6 @@ key_file = File.join("config", "master.key")
 if File.exist?(key_file)
   ENV["RAILS_MASTER_KEY"] = File.read(key_file)
 end
-
-p ENV["RAILS_MASTER_KEY"]
-# p Rails.application.credentials.gcp[:db_password]
-# else
-#   client = Google::Cloud::SecretManager.secret_manager_service
-
-#   master_key = client.secret_version_path(
-#     project:        'trambui-test-1',
-#     secret:         'rails-master-key',
-#     secret_version: 'latest'
-#   )
-#   version = client.access_secret_version name: master_key
-#   payload = version.payload.data
-#   ENV["RAILS_MASTER_KEY"] = payload
-# end
-
-# to get access to RAILS_MASTER_KEY during apply migrations build step
-# if ENV["RAILS_MASTER_KEY_SECRET"].present?
-#   require "google/cloud/secret_manager/v1"
-#   client = ::Google::Cloud::SecretManager::V1::SecretManagerService::Client.new
-#   value = client.access_secret_version(name: ENV["RAILS_MASTER_KEY_SECRET"]).payload.data
-#   ENV["RAILS_MASTER_KEY"] ||= value # could also just "=" here
-# end
 
 module RailsCatAlbum
   class Application < Rails::Application
