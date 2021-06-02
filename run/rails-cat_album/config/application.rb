ab2eb48ab571d27092b3feeeacb993d9a0a80592
Tram Bui <62119967+trambui09@users.noreply.github.com>
2021-06-01 19:59:05 -0500

    chore(run): update cat_album to have Dockerfile, Cloudbuild.yaml, and proper database configurations (#809)

--- run/rails-cat_album/config/application.rb
+++ run/rails-cat_album/config/application.rb
@@ -7,17 +7,19 @@ require "active_job/railtie"
 require "active_record/railtie"
 require "active_storage/engine"
 require "action_controller/railtie"
-# require "action_mailer/railtie"
-# require "action_mailbox/engine"
-# require "action_text/engine"
 require "action_view/railtie"
-# require "action_cable/engine"
 require "sprockets/railtie"
 require "rails/test_unit/railtie"
+
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
+
+key_file = File.join "config", "master.key"
+if File.exist? key_file
+  ENV["RAILS_MASTER_KEY"] = File.read key_file
+end
 
 module RailsCatAlbum
   class Application < Rails::Application
