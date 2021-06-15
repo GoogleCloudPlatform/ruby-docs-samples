7aaefded514aaa3534d0cb6084b6ddefb9bdf660
Tram Bui <trambui@google.com>
2021-06-15 12:24:18 -0500

    extracted the views image url into a helper method

--- run/rails-cat_album/app/helpers/photos_helper.rb
+++ run/rails-cat_album/app/helpers/photos_helper.rb
@@ -1,2 +1,11 @@
 module PhotosHelper
+  def image_url photo
+    case Rails.application.config.active_storage.service
+    when :local
+      url = url_for photo.image
+    when :google
+      url = "https://storage.googleapis.com/cat_album_storage/#{photo.image.key}"
+    end
+    url
+  end
 end
