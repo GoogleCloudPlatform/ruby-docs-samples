cc592f1ac0b1e6973967ee1da48acb7571168971
Tram Bui <trambui@google.com>
2021-06-16 16:50:10 -0500

    refactored image_url helper method and cleaned up storage.yml

--- run/rails-cat_album/app/helpers/photos_helper.rb
+++ run/rails-cat_album/app/helpers/photos_helper.rb
@@ -2,10 +2,9 @@ module PhotosHelper
   def image_url photo
     case Rails.application.config.active_storage.service
     when :local
-      url = url_for photo.image
+      url_for photo.image
     when :google
-      url = "https://storage.googleapis.com/cat_album_storage/#{photo.image.key}"
+      "https://storage.googleapis.com/cat_album_storage/#{photo.image.key}"
     end
-    url
   end
 end
