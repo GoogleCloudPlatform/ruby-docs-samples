f82e106a40dec938dabf735715678203e2f4b5bc
Tram Bui <62119967+trambui09@users.noreply.github.com>
2021-06-23 17:05:51 -0500

    docs(run): connect cloud storage to cat_album (#813)

--- run/rails-cat_album/app/helpers/photos_helper.rb
+++ run/rails-cat_album/app/helpers/photos_helper.rb
@@ -1,2 +1,10 @@
 module PhotosHelper
+  def image_url photo
+    case Rails.application.config.active_storage.service
+    when :local
+      url_for photo.image
+    when :google
+      "https://storage.googleapis.com/cat_album_storage/#{photo.image.key}"
+    end
+  end
 end
