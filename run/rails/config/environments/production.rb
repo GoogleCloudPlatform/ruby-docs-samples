5fb50b2f84534d1484e9e2056fb8696328560662
Tram Bui <62119967+trambui09@users.noreply.github.com>
2021-07-15 16:46:21 -0500

    chore(run): add region tags to rails cloud run sample app (#835)
    Add region tags to files in Rails Cloud Run sample app to use as snippets in documentation

--- run/rails/config/environments/production.rb
+++ run/rails/config/environments/production.rb
@@ -39,8 +39,10 @@ Rails.application.configure do
   # config.action_dispatch.x_sendfile_header = 'X-Sendfile' # for Apache
   # config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect' # for NGINX
 
+  # [START cloudrun_rails_storage_prod]
   # Store uploaded files on the local file system (see config/storage.yml for options).
   config.active_storage.service = :google
+  # [END cloudrun_rails_storage_prod]
 
   # Mount Action Cable outside main process or domain.
   # config.action_cable.mount_path = nil
