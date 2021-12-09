705c77582b7392b092b5636d07c6f0a09afec06e
Nivedha <nivedhasenthil@gmail.com>
2021-12-09 18:00:13 +0530

    update add_timestamp_column

--- spanner/spanner_samples.rb
+++ spanner/spanner_samples.rb
@@ -503,12 +503,16 @@ def add_timestamp_column project_id:, instance_id:, database_id:
   # database_id = "Your Spanner database ID"
 
   require "google/cloud/spanner"
+  require "google/cloud/spanner/admin/database"
 
-  spanner  = Google::Cloud::Spanner.new project: project_id
-  instance = spanner.instance instance_id
-  database = instance.database database_id
+  database_admin_client = Google::Cloud::Spanner::Admin::Database.database_admin
 
-  job = database.update statements: [
+  db_path = database_admin_client.database_path project: project_id,
+                                   instance: instance_id,
+                                   database: database_id
+
+  job = database_admin_client.update_database_ddl database: db_path,
+   statements: [
     "ALTER TABLE Albums ADD COLUMN LastUpdateTime TIMESTAMP
      OPTIONS (allow_commit_timestamp=true)"
   ]
