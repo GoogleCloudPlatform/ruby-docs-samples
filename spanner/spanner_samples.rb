5748a8d5cc002bbb067510c313e5b48bc2459843
Nivedha <nivedhasenthil@gmail.com>
2021-12-09 18:46:29 +0530

    update write_read_float64_array

--- spanner/spanner_samples.rb
+++ spanner/spanner_samples.rb
@@ -1823,20 +1823,26 @@ def write_read_float64_array project_id:, instance_id:, database_id:
   # database_id = "Your Spanner database ID"
 
   require "google/cloud/spanner"
+  require "google/cloud/spanner/admin/database"
   require "securerandom"
 
-  spanner = Google::Cloud::Spanner.new project: project_id
-  database = spanner.database instance_id, database_id
-  job = database.update statements: [
-    "CREATE TABLE Boxes (
-        BoxId             STRING(36) NOT NULL,
-        Heights           ARRAY<INT64>,
-        Weights           ARRAY<FLOAT64>,
-        ErrorChecks       ARRAY<BOOL>
-      ) PRIMARY KEY (BoxId)"
-  ]
-  job.wait_until_done!
+  database_admin_client = Google::Cloud::Spanner::Admin::Database.database_admin
 
+  db_path = database_admin_client.database_path project: project_id,
+                                   instance: instance_id,
+                                   database: database_id
+
+  job = database_admin_client.update_database_ddl database: db_path, statements: [
+    "CREATE TABLE Boxes (
+      BoxId             STRING(36) NOT NULL,
+      Heights           ARRAY<INT64>,
+      Weights           ARRAY<FLOAT64>,
+      ErrorChecks       ARRAY<BOOL>
+      ) PRIMARY KEY (BoxId)"
+    ]
+  job.wait_until_done!
+    
+  spanner = Google::Cloud::Spanner.new project: project_id
   client = spanner.client instance_id, database_id
 
   box_id = SecureRandom.uuid
