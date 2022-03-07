e9c9642e0e8bb57466de9ad84a3c630af6c01c1d
igorpeshansky <igorpeshansky@users.noreply.github.com>
2022-03-07 06:09:51 -0500

    Fix deprecated logging agent install script. (#933)
    See https://cloud.google.com/logging/docs/agent/installation for the latest installation instructions.

--- getting-started/gce/startup-script.sh
+++ getting-started/gce/startup-script.sh
@@ -14,8 +14,8 @@
 
 # [START getting_started_gce_startup_script]
 # Install Stackdriver logging agent
-curl -sSO https://dl.google.com/cloudagents/install-logging-agent.sh
-bash install-logging-agent.sh
+curl -sSO https://dl.google.com/cloudagents/add-logging-agent-repo.sh
+bash add-logging-agent-repo.sh --also-install
 
 # Install dependencies
 apt-get update && apt-get -y upgrade && apt-get install -y autoconf bison \
