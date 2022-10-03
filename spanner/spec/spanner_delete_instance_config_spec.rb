cdfb201f97739979c6d7f9328e90e2c952a919ad
rahul2393 <irahul@google.com>
2022-10-03 09:05:04 +0530

    feat(spanner): add support for CMMR phase 2 (#1014)

---
+++ spanner/spec/spanner_delete_instance_config_spec.rb
@@ -0,0 +1,33 @@
+# Copyright 2022 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+require_relative "../spanner_create_instance_config"
+require_relative "../spanner_delete_instance_config"
+require_relative "./spec_helper"
+
+describe "Spanner custom instance config" do
+  example "Delete" do
+    base_config_id = instance_config_path("nam7")
+    user_config = "custom-ruby-samples-config-#{SecureRandom.hex(8)}"
+    spanner_create_instance_config project_id: @project_id,
+                                   user_config_name: user_config,
+                                   base_config_id: base_config_id
+    capture do
+      spanner_delete_instance_config user_config_id: instance_config_path(user_config)
+    end
+    expect(captured_output).to include(
+                                 "Deleted instance configuration #{instance_config_path(user_config)}"
+                               )
+  end
+end
