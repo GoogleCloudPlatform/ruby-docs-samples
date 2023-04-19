b4dd7a8cd3a7b2d06b1ebb9e5d12d38b8f51ee57
Pratiksha Kap <pratikshakap@google.com>
2023-04-19 12:39:50 -0700

    remove invalid tag

--- appengine/flexible/ruby31-and-earlier/rails-hello_world/config/routes.rb
+++ appengine/flexible/ruby31-and-earlier/rails-hello_world/config/routes.rb
@@ -12,21 +12,17 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-# [START welcome_routes]
 Rails.application.routes.draw do
   get "welcome/index"
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root "welcome#index"
 end
-# [END welcome_routes]
 
 =begin
-# [START boilerplate]
 Rails.application.routes.draw do
   get 'welcome/index'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
-[END boilerplate]
 =end
