a61e1acb2dc12f4e6552c71a3b70a80bf26162f6
janell-chen <122311137+janell-chen@users.noreply.github.com>
2023-05-31 14:27:30 -0700

    Add response streaming sample in ruby

---
+++ functions/response_streaming/app.rb
@@ -0,0 +1,38 @@
+# Copyright 2023 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# [START functions_response_streaming]
+
+require "functions_framework"
+require "cgi"
+require "json"
+require "google/cloud/bigquery"
+
+FunctionsFramework.http "stream_big_query" do |request|
+  bigquery = Google::Cloud::Bigquery.new
+  sql = "SELECT abstract FROM `bigquery-public-data.breathe.bioasq` LIMIT 1000"
+
+  results = bigquery.query sql do |config|
+    config.location = "US"
+  end
+
+  body = Enumerator.new do |y|
+    results.each do |row|
+      y << "#{row}\n"
+    end
+  end
+  [200, {}, body]
+end
+
+# [END functions_response_streaming]
