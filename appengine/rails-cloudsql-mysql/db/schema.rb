e250e806173abd8eadd61fc52722ddbe0033f5c9
Shiv Gautam <shivng@google.com>
2023-09-19 08:52:38 +0000

    chore(samples): upgraded to rails 7 for mysql component

--- appengine/rails-cloudsql-mysql/db/schema.rb
+++ appengine/rails-cloudsql-mysql/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define version: 20_170_804_210_744 do
+ActiveRecord::Schema[6.1].define version: 20_170_804_210_744 do
   create_table "cats", force: :cascade do |t|
     t.string "name"
     t.integer "age"
