71e85589b669fc3af0664b14360507d1b2bf155b
Shiv Gautam <85628657+shivgautam@users.noreply.github.com>
2023-10-06 22:26:18 +0530

    Rails7 samples update cloudsql sqlserver (#1178)

--- cloud-sql/sqlserver/activerecord/config/application.rb
+++ cloud-sql/sqlserver/activerecord/config/application.rb
@@ -9,11 +9,14 @@ Bundler.require(*Rails.groups)
 module Activerecord
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 5.2
+    config.load_defaults 7.0
 
-    # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration can go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded after loading
-    # the framework and any gems in your application.
+    # Configuration for the application, engines, and railties goes here.
+    #
+    # These settings can be overridden in specific environments using the files
+    # in config/environments, which are processed later.
+    #
+    # config.time_zone = "Central Time (US & Canada)"
+    # config.eager_load_paths << Rails.root.join("extras")
   end
 end
