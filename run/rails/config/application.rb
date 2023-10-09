a97a456bc657ae955f710fd0e674763fc7d138b2
Shiv Gautam <shivng@google.com>
2023-10-09 04:29:55 +0000

    chore(samples): rails 7 updatesf for run/rails

--- run/rails/config/application.rb
+++ run/rails/config/application.rb
@@ -7,8 +7,11 @@ require "active_job/railtie"
 require "active_record/railtie"
 require "active_storage/engine"
 require "action_controller/railtie"
+# require "action_mailer/railtie"
+# require "action_mailbox/engine"
+# require "action_text/engine"
 require "action_view/railtie"
-require "sprockets/railtie"
+require "action_cable/engine"
 require "rails/test_unit/railtie"
 
 
@@ -24,7 +27,7 @@ end
 module RailsCatAlbum
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 6.1
+    config.load_defaults 7.0
 
     # Configuration for the application, engines, and railties goes here.
     #
