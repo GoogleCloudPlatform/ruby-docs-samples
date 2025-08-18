e584e26730ce3019127571054db3d40937593dd8
Katie McLaughlin <katie@glasnt.com>
2025-08-18 10:17:01 +1000

    automated rails app:update on rails 5.x

--- getting-started/bookshelf/config/initializers/wrap_parameters.rb
+++ getting-started/bookshelf/config/initializers/wrap_parameters.rb
@@ -17,11 +17,6 @@
 # is enabled by default.
 
 # Enable parameter wrapping for JSON. You can disable this by setting :format to an empty array.
-ActiveSupport.on_load :action_controller do
+ActiveSupport.on_load(:action_controller) do
   wrap_parameters format: [:json]
 end
-
-# To enable root element in JSON for ActiveRecord objects.
-# ActiveSupport.on_load(:active_record) do
-#   self.include_root_in_json = true
-# end
